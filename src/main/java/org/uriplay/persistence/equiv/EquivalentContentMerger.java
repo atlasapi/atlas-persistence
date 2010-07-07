3d786ad5610944e090e1b4278c8fd47ffeb074ab
John Ayres <john@metabroadcast.com>
2010-07-07 09:44:09 +0000

    merge content
    git-svn-id: https://uriplay.googlecode.com/svn/uriplay-persistence/trunk@1213 9f0b3f36-cced-11dd-a405-31afbefa9e0e

--- src/main/java/org/uriplay/persistence/equiv/EquivalentContentMerger.java
+++ src/main/java/org/uriplay/persistence/equiv/EquivalentContentMerger.java
@@ -4,9 +4,14 @@ import java.util.List;
 
 import org.uriplay.media.entity.Brand;
 import org.uriplay.media.entity.Content;
+import org.uriplay.media.entity.Encoding;
+import org.uriplay.media.entity.Episode;
 import org.uriplay.media.entity.Item;
 import org.uriplay.media.entity.Playlist;
+import org.uriplay.media.entity.Version;
 
+import com.google.common.base.Strings;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
 public class EquivalentContentMerger {
@@ -45,7 +50,43 @@ public class EquivalentContentMerger {
 		return brand;
 	}
 
+	@SuppressWarnings("unchecked")
 	private void mergeSubItemsOf(Brand original, Brand equivTo) {
-		// TODO
+		for (Item item : original.getItems()) {
+			Version version = Iterables.get(item.getVersions(), 0, null);
+			if (version == null) {
+				continue;
+			}
+			Item sameItem = findSameItem((Episode) item, (List) equivTo.getItems());
+			if (sameItem != null && !sameItem.getVersions().isEmpty()) {
+				for (Encoding encoding : Iterables.get(sameItem.getVersions(), 0).getManifestedAs()) {
+					version.addManifestedAs(encoding);
+				}
+			}
+		}
+	}
+
+	private Item findSameItem(Episode needle, List<Episode> haystack) {
+		for (Episode item : haystack) {
+			if (areSame(needle, item)) {
+				return item;
+			}
+		}
+		return null;
+	}
+
+	private boolean areSame(Episode ep1, Episode ep2) {
+		return sameSeriesAndEpisodeNumber(ep1, ep2) || sameTitle(ep1, ep2);
+	}
+
+	private boolean sameTitle(Item ep1, Item ep2) {
+		return Strings.nullToEmpty(ep1.getTitle()).equals(ep2.getTitle());
+	}
+
+	private boolean sameSeriesAndEpisodeNumber(Episode ep1, Episode ep2) {
+		if (ep1.getEpisodeNumber() == null || ep1.getSeriesNumber() == null) {
+			return false;
+		}
+		return ep1.getSeriesNumber().equals(ep2.getSeriesNumber()) && ep1.getEpisodeNumber().equals(ep2.getEpisodeNumber());
 	}
 }
