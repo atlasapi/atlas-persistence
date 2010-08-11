8bd24f1875e5521bf25ecc1f1352727a75c9a108
John Ayres <john@metabroadcast.com>
2010-08-11 11:12:00 +0100

    persist series

--- src/main/java/org/atlasapi/persistence/media/entity/EpisodeTranslator.java
+++ src/main/java/org/atlasapi/persistence/media/entity/EpisodeTranslator.java
@@ -2,15 +2,20 @@ package org.atlasapi.persistence.media.entity;
 
 import org.atlasapi.media.entity.Brand;
 import org.atlasapi.media.entity.Episode;
+import org.atlasapi.media.entity.Series;
 import org.atlasapi.persistence.ModelTranslator;
+import org.atlasapi.persistence.media.entity.SeriesTranslator.SeriesSummaryTranslator;
 
 import com.metabroadcast.common.persistence.translator.TranslatorUtils;
 import com.mongodb.DBObject;
 
 public class EpisodeTranslator implements ModelTranslator<Episode> {
    
+	private static final String EMBEDDED_SERIES_KEY = "series";
+	
 	private final ItemTranslator itemTranslator = new ItemTranslator();
     private final BrandTranslator brandTranslator = new BrandTranslator();
+    private final SeriesSummaryTranslator seriesTranslator = new SeriesTranslator.SeriesSummaryTranslator();
     
     @Override
     public Episode fromDBObject(DBObject dbObject, Episode entity) {
@@ -27,7 +32,10 @@ public class EpisodeTranslator implements ModelTranslator<Episode> {
             Brand brand = brandTranslator.fromDBObject((DBObject) dbObject.get("brand"), null);
             entity.setBrand(brand);
         }
-        
+        if (dbObject.containsField(EMBEDDED_SERIES_KEY)) {
+        	 Series series = seriesTranslator.fromDBObject((DBObject) dbObject.get(EMBEDDED_SERIES_KEY));
+             entity.setSeries(series);
+        }
         return entity;
     }
 
@@ -42,6 +50,12 @@ public class EpisodeTranslator implements ModelTranslator<Episode> {
             DBObject brand = brandTranslator.toDBObjectForEmbeddedBrand(null, entity.getBrand());
             dbObject.put("brand", brand);
         }
+        
+        if (entity.getSeries() != null) {
+        	DBObject series = seriesTranslator.toDBObjectForSummary(entity.getSeries());
+            dbObject.put(EMBEDDED_SERIES_KEY, series);
+        }
+        
         dbObject.put("type", Episode.class.getSimpleName());
         
         return dbObject;
---
+++ src/main/java/org/atlasapi/persistence/media/entity/SeriesTranslator.java
@@ -0,0 +1,53 @@
+package org.atlasapi.persistence.media.entity;
+
+import org.atlasapi.media.entity.Series;
+
+import com.mongodb.DBObject;
+
+public class SeriesTranslator {
+    
+	private static final String SERIES_NUMBER_KEY = "seriesNumber";
+	
+	private final PlaylistTranslator playlistTranslator ;
+
+	public SeriesTranslator() {
+		this(new PlaylistTranslator());
+	}
+	
+	public SeriesTranslator(PlaylistTranslator playlistTranslator) {
+		this.playlistTranslator = playlistTranslator;
+	}
+	
+    public Series fromDBObject(DBObject dbObject) {
+        Series entity = new Series();
+        playlistTranslator.fromDBObject(dbObject, entity);
+        entity.withSeriesNumber((Integer) dbObject.get(SERIES_NUMBER_KEY));
+        return entity;
+    }
+
+    public DBObject toDBObject(Series entity) {
+        DBObject dbObject = playlistTranslator.toDBObject(null, entity);
+        if (entity.getSeriesNumber() != null) {
+        	dbObject.put(SERIES_NUMBER_KEY, entity.getSeriesNumber());
+        }
+        return dbObject;
+    }
+
+    
+   static class SeriesSummaryTranslator {
+    	
+    	private final SeriesTranslator seriesTranslator;
+
+		public SeriesSummaryTranslator() {
+    		this.seriesTranslator = new SeriesTranslator(new PlaylistTranslator(new ContentTranslator(new DescriptionTranslator(false))));
+    	}
+		
+		public DBObject toDBObjectForSummary(Series series) {
+			return seriesTranslator.toDBObject(series);
+		}
+
+		public Series fromDBObject(DBObject dbObject) {
+			return seriesTranslator.fromDBObject(dbObject);
+		}
+    }
+}
