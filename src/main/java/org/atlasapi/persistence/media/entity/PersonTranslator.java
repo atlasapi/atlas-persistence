77a59ba5de4f7751f7027d400a4e64b4d18abba8
Ben Smith <ben@thesmith.co.uk>
2011-02-17 23:25:30 +0000

    added uri/curie/publisher to person translator

--- src/main/java/org/atlasapi/persistence/media/entity/PersonTranslator.java
+++ src/main/java/org/atlasapi/persistence/media/entity/PersonTranslator.java
@@ -3,6 +3,7 @@ package org.atlasapi.persistence.media.entity;
 import org.atlasapi.media.entity.Actor;
 import org.atlasapi.media.entity.CrewMember;
 import org.atlasapi.media.entity.Person;
+import org.atlasapi.media.entity.Publisher;
 import org.atlasapi.media.entity.CrewMember.Role;
 import org.atlasapi.persistence.ModelTranslator;
 
@@ -16,13 +17,16 @@ public class PersonTranslator implements ModelTranslator<Person> {
     @Override
     public Person fromDBObject(DBObject dbObject, Person model) {
         String type = (String) dbObject.get("type");
+        String uri = (String) dbObject.get("uri");
+        String curie = (String) dbObject.get("curie");
+        Publisher publisher = Publisher.fromKey((String) dbObject.get("publisher")).requireValue();
         
         Person person = null;
         if (type.equals(Actor.class.getSimpleName())) {
-            person = new Actor().withCharacter((String) dbObject.get("character"));
+            person = new Actor(uri, curie, publisher).withCharacter((String) dbObject.get("character"));
         } else if (type.equals(CrewMember.class.getSimpleName())) {
             Role role = Role.fromKey((String) dbObject.get("role"));
-            person = new CrewMember().withRole(role);
+            person = new CrewMember(uri, curie, publisher).withRole(role);
         }
         person.withProfileLink((String) dbObject.get("profileLink")).withName((String) dbObject.get("name"));
         descriptionTranslator.fromDBObject(dbObject, person);
@@ -37,6 +41,9 @@ public class PersonTranslator implements ModelTranslator<Person> {
         dbObject.put("type", model.getClass().getSimpleName());
         TranslatorUtils.from(dbObject, "name", model.name());
         TranslatorUtils.from(dbObject, "profileLink", model.profileLink());
+        TranslatorUtils.from(dbObject, "uri", model.getCanonicalUri());
+        TranslatorUtils.from(dbObject, "curie", model.getCurie());
+        TranslatorUtils.from(dbObject, "publisher", model.getPublisher().key());
         
         if (model instanceof Actor) {
             Actor actor = (Actor) model;
