34e57b9d62d08a5c25145af49d49ce1aba21a981
John Ayres <john@metabroadcast.com>
2011-03-07 16:09:37 +0000

    run schedule rebuild weekly

--- src/main/java/org/atlasapi/persistence/MongoContentPersistenceModule.java
+++ src/main/java/org/atlasapi/persistence/MongoContentPersistenceModule.java
@@ -1,5 +1,7 @@
 package org.atlasapi.persistence;
 
+import javax.annotation.PostConstruct;
+
 import org.atlasapi.persistence.content.AggregateContentListener;
 import org.atlasapi.persistence.content.ContentWriter;
 import org.atlasapi.persistence.content.EventFiringContentWriter;
@@ -9,17 +11,26 @@ import org.atlasapi.persistence.content.mongo.MongoScheduleStore;
 import org.atlasapi.persistence.content.mongo.ScheduleUpdatingContentListener;
 import org.atlasapi.persistence.shorturls.MongoShortUrlSaver;
 import org.atlasapi.persistence.shorturls.ShortUrlSaver;
+import org.joda.time.LocalTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
+import com.metabroadcast.common.scheduling.RepetitionRules;
+import com.metabroadcast.common.scheduling.SimpleScheduler;
+import com.metabroadcast.common.time.DayOfWeek;
 
 @Configuration
 public class MongoContentPersistenceModule implements ContentPersistenceModule {
 
 	private @Autowired DatabasedMongo db;
+	private @Autowired SimpleScheduler scheduler;
 	
+	@PostConstruct
+	public void installScheduleRebuilder() {
+		scheduler.schedule(scheduleRepopulator(), RepetitionRules.weekly(DayOfWeek.SUNDAY, new LocalTime(1, 0, 0)));
+	}
 	
 	public @Bean ContentWriter persistentWriter() {
 		return eventFiringContentWriter();
