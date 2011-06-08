37330723a2d50bcfcc13ca7f82646b5466d5c156
Fred van den Driessche <fred@metabroadcast.com>
2011-06-08 12:38:16 +0100

    only insert non-existent lookup entries

--- src/main/java/org/atlasapi/persistence/lookup/mongo/MongoLookupEntryStore.java
+++ src/main/java/org/atlasapi/persistence/lookup/mongo/MongoLookupEntryStore.java
@@ -1,16 +1,27 @@
 package org.atlasapi.persistence.lookup.mongo;
 
+import static com.metabroadcast.common.persistence.mongo.MongoConstants.ID;
 import static org.atlasapi.persistence.lookup.entry.LookupEntry.lookupEntryFrom;
+
+import java.util.List;
+import java.util.Set;
 
 import org.atlasapi.media.entity.Described;
 import org.atlasapi.persistence.lookup.NewLookupWriter;
 import org.atlasapi.persistence.lookup.entry.LookupEntry;
 import org.atlasapi.persistence.lookup.entry.LookupEntryStore;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
 import com.metabroadcast.common.persistence.mongo.MongoBuilders;
+import com.metabroadcast.common.persistence.mongo.MongoConstants;
 import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
 
 public class MongoLookupEntryStore implements LookupEntryStore, NewLookupWriter {
 
@@ -36,7 +47,31 @@ public class MongoLookupEntryStore implements LookupEntryStore, NewLookupWriter 
 
     @Override
     public void ensureLookup(Described described) {
-        lookup.insert(Lists.transform(lookupEntryFrom(described).entriesForIdentifiers(),translator.TO_DBO));
+        List<LookupEntry> entriesForIdentifiers = lookupEntryFrom(described).entriesForIdentifiers();
+        
+        Set<String> existing = getIds(MongoBuilders.where().fieldIn(MongoConstants.ID, Iterables.transform(entriesForIdentifiers, LookupEntry.TO_ID)).find(lookup));
+        
+        lookup.insert(Lists.transform(filterExisting(entriesForIdentifiers, existing),translator.TO_DBO));
+    }
+
+    private List<LookupEntry> filterExisting(List<LookupEntry> entriesForIdentifiers, final Set<String> existingIds) {
+        
+        return ImmutableList.copyOf(Iterables.filter(entriesForIdentifiers, new Predicate<LookupEntry>() {
+            @Override
+            public boolean apply(LookupEntry input) {
+                return existingIds.contains(input.id());
+            }
+        }));
+    }
+
+    private Set<String> getIds(Iterable<DBObject> existing) {
+        final Set<String> existingIds = ImmutableSet.copyOf(Iterables.transform(existing, new Function<DBObject, String>() {
+            @Override
+            public String apply(DBObject input) {
+                return (String) input.get(ID);
+            }
+        }));
+        return existingIds;
     }
 
 }
