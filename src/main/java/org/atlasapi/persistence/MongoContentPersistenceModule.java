45128b3cec38d5d2f044f14763097e18c937257a
Fred van den Driessche <fred@metabroadcast.com>
2011-06-09 12:14:29 +0100

    cleaned up content persistence module

--- src/main/java/org/atlasapi/persistence/MongoContentPersistenceModule.java
+++ src/main/java/org/atlasapi/persistence/MongoContentPersistenceModule.java
@@ -1,19 +1,21 @@
 package org.atlasapi.persistence;
 
-import org.atlasapi.persistence.content.AggregateContentListener;
+import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ContentWriter;
-import org.atlasapi.persistence.content.EventFiringContentWriter;
-import org.atlasapi.persistence.content.mongo.AsyncronousContentListener;
-import org.atlasapi.persistence.content.mongo.MongoDbBackedContentStore;
+import org.atlasapi.persistence.content.mongo.KnownTypeContentResolver;
+import org.atlasapi.persistence.content.mongo.LookupResolvingContentResolver;
+import org.atlasapi.persistence.content.mongo.MongoContentResolver;
+import org.atlasapi.persistence.content.mongo.MongoContentWriter;
 import org.atlasapi.persistence.content.mongo.MongoPersonStore;
 import org.atlasapi.persistence.content.people.ItemsPeopleWriter;
 import org.atlasapi.persistence.content.people.QueuingItemsPeopleWriter;
 import org.atlasapi.persistence.content.people.QueuingPersonWriter;
 import org.atlasapi.persistence.content.schedule.mongo.BatchingScheduleWriter;
 import org.atlasapi.persistence.content.schedule.mongo.MongoScheduleStore;
-import org.atlasapi.persistence.content.schedule.mongo.ScheduleUpdatingContentListener;
 import org.atlasapi.persistence.content.schedule.mongo.ScheduleWriter;
 import org.atlasapi.persistence.logging.AdapterLog;
+import org.atlasapi.persistence.lookup.BasicLookupResolver;
+import org.atlasapi.persistence.lookup.mongo.MongoLookupEntryStore;
 import org.atlasapi.persistence.shorturls.MongoShortUrlSaver;
 import org.atlasapi.persistence.shorturls.ShortUrlSaver;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +23,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
+import com.metabroadcast.common.time.SystemClock;
 
 @Configuration
 public class MongoContentPersistenceModule implements ContentPersistenceModule {
@@ -28,12 +31,20 @@ public class MongoContentPersistenceModule implements ContentPersistenceModule {
 	private @Autowired DatabasedMongo db;
 	private @Autowired AdapterLog log;
 	
-	public @Bean ContentWriter persistentWriter() {
-		return eventFiringContentWriter();
+	public @Bean ContentWriter contentWriter() {
+		return new MongoContentWriter(db, lookupStore(), new SystemClock());
 	}
 	
-	public @Bean(name={"mongoContentStore"}) MongoDbBackedContentStore contentStore() {
-		return new MongoDbBackedContentStore(db);
+	public @Bean ContentResolver contentResolver() {
+	    return new LookupResolvingContentResolver(knownTypeContentResolver(), new BasicLookupResolver(lookupStore()));
+	}
+	
+	public @Bean KnownTypeContentResolver knownTypeContentResolver() {
+	    return new MongoContentResolver(db);
+	}
+	
+	public @Bean MongoLookupEntryStore lookupStore() {
+	    return new MongoLookupEntryStore(db);
 	}
 	
 	public @Bean MongoScheduleStore scheduleStore() {
@@ -55,20 +66,10 @@ public class MongoContentPersistenceModule implements ContentPersistenceModule {
 	public @Bean ScheduleWriter queueingScheduleWriter() {
 		return new BatchingScheduleWriter(scheduleStore(), log);
 	}
-	
-
-	@Bean EventFiringContentWriter eventFiringContentWriter() {
-	    return new EventFiringContentWriter(contentStore(), contentListener());
-	}
-
-	public @Bean AggregateContentListener contentListener() {
-	    AggregateContentListener listener = new AggregateContentListener();
-	    listener.addListener(new AsyncronousContentListener(new ScheduleUpdatingContentListener(queueingScheduleWriter()), log));
-	    return listener;
-	}
 
 	@Override
 	public @Bean ShortUrlSaver shortUrlSaver() {
 		return new MongoShortUrlSaver(db);
 	}
+
 }
