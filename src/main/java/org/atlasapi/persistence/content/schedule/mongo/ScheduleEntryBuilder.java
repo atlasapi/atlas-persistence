35c8e5daa6db8c9b561c50cc7146d149cdb1cb93
Tom McAdam <tom@metabroadcast.com>
2011-10-03 09:47:16 +0100

    Add method to allow update to a portion of the schedule

--- src/main/java/org/atlasapi/persistence/content/schedule/mongo/ScheduleEntryBuilder.java
+++ src/main/java/org/atlasapi/persistence/content/schedule/mongo/ScheduleEntryBuilder.java
@@ -1,5 +1,6 @@
 package org.atlasapi.persistence.content.schedule.mongo;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,7 +17,6 @@ import org.joda.time.Interval;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Maps;
 import com.metabroadcast.common.time.DateTimeZones;
 
 public class ScheduleEntryBuilder {
@@ -25,37 +25,43 @@ public class ScheduleEntryBuilder {
     private static final Duration MAX_BROADCAST_AGE = Duration.standardDays(28);
     
     public Map<String, ScheduleEntry> toScheduleEntries(Iterable<? extends Item> items) {
-        Map<String, ScheduleEntry> entries = Maps.newHashMap();
-
+    	Map<String, ScheduleEntry> entries = new HashMap<String, ScheduleEntry>();
+    	
         for (Item item : items) {
             for (Version version : item.nativeVersions()) {
-                for (Broadcast broadcast : version.getBroadcasts()) {
-                    if(!broadcast.isActivelyPublished() && Publisher.PA.equals(version.getProvider())) {
-                        continue;
-                    }
-                    
+                for (Broadcast broadcast : version.getBroadcasts()) {  
                     ItemRefAndBroadcast itemAndBroadcast = new ItemRefAndBroadcast(item, broadcast);
                     
                     Channel channel = Channel.fromUri(broadcast.getBroadcastOn()).requireValue();
                     Publisher publisher = item.getPublisher();
                     
-                    for (Interval interval: intervalsFor(broadcast.getTransmissionTime(), broadcast.getTransmissionEndTime())) {
-                        String key = ScheduleEntry.toKey(interval, channel, publisher);
-
-                        if (entries.containsKey(key)) {
-                            ScheduleEntry entry = entries.get(key);
-                            entry.withItems(Iterables.concat(entry.getItemRefsAndBroadcasts(), ImmutableList.of(itemAndBroadcast)));
-                        } else {
-                            ScheduleEntry entry = new ScheduleEntry(interval, channel, publisher, ImmutableList.of(itemAndBroadcast));
-                            entries.put(key, entry);
-                        }
-                    }
+                    toScheduleEntryFromBroadcast(channel, publisher, itemAndBroadcast, entries);
                 }
             }
         }
 
         return entries;
     }
+
+	public void toScheduleEntryFromBroadcast(Channel channel, Publisher publisher, ItemRefAndBroadcast itemAndBroadcast, Map<String, ScheduleEntry> entries) {
+	    
+		Broadcast broadcast = itemAndBroadcast.getBroadcast();
+		if(!broadcast.isActivelyPublished() && Publisher.PA.equals(publisher)) {
+			return;
+		}
+		
+	    for (Interval interval: intervalsFor(broadcast.getTransmissionTime(), broadcast.getTransmissionEndTime())) {
+	        String key = ScheduleEntry.toKey(interval, channel, publisher);
+
+	        if (entries.containsKey(key)) {
+	            ScheduleEntry entry = entries.get(key);
+	            entry.withItems(Iterables.concat(entry.getItemRefsAndBroadcasts(), ImmutableList.of(itemAndBroadcast)));
+	        } else {
+	            ScheduleEntry entry = new ScheduleEntry(interval, channel, publisher, ImmutableList.of(itemAndBroadcast));
+	            entries.put(key, entry);
+	        }
+	    }
+	}
     
     List<Interval> intervalsFor(DateTime start, DateTime end) {
         if (start.isBefore(new DateTime(DateTimeZones.UTC).minus(MAX_BROADCAST_AGE))) {
