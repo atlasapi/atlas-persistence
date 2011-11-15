a865069ce4485f83f5be4a84b8a27b6e8ee1e3de
Fred van den Driessche <fred@metabroadcast.com>
2011-11-15 10:54:50 +0000

    persist key phrases

--- src/main/java/org/atlasapi/persistence/media/entity/ContentTranslator.java
+++ src/main/java/org/atlasapi/persistence/media/entity/ContentTranslator.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.atlasapi.media.entity.Clip;
 import org.atlasapi.media.entity.Content;
+import org.atlasapi.media.entity.KeyPhrase;
 import org.atlasapi.persistence.ModelTranslator;
 
+import com.google.common.base.Function;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
@@ -13,30 +17,49 @@ import com.mongodb.DBObject;
 
 public class ContentTranslator implements ModelTranslator<Content> {
 
-	public static String CLIPS_KEY = "clips";
+    private static final String PHRASES_KEY = "phrases";
+    public static final String CLIPS_KEY = "clips";
 	
-	private ClipTranslator clipTranslator;
+	private final ClipTranslator clipTranslator;
+	private final KeyPhraseTranslator keyPhraseTranslator;
 	private final DescribedTranslator describedTranslator;
 	
 	public ContentTranslator() {
 		this(new DescribedTranslator(new DescriptionTranslator()), new ClipTranslator());
 	}
 	
+	//TODO: why not use collaborators interface here? ModelTranslator<Described> etc...
 	public ContentTranslator(DescribedTranslator describedTranslator, ClipTranslator clipTranslator) {
 		this.describedTranslator = describedTranslator;
 		this.clipTranslator = clipTranslator;
-	}
-	
-	public void setClipTranslator(ClipTranslator clipTranslator) {
-		this.clipTranslator = clipTranslator;
+		this.keyPhraseTranslator = new KeyPhraseTranslator();
 	}
 	
 	@Override
-	@SuppressWarnings("unchecked")
 	public Content fromDBObject(DBObject dbObject, Content entity) {
 		describedTranslator.fromDBObject(dbObject, entity);
 
-		if (dbObject.containsField(CLIPS_KEY)) {
+		decodeClips(dbObject, entity);
+        decodeKeyPhrases(dbObject, entity);
+
+		return entity;
+	}
+
+	@SuppressWarnings("unchecked")
+    private void decodeKeyPhrases(DBObject dbObject, Content entity) {
+        if (dbObject.containsField(PHRASES_KEY)) {
+            entity.setKeyPhrases(Iterables.transform((Iterable<DBObject>) dbObject.get(PHRASES_KEY), new Function<DBObject, KeyPhrase>() {
+                @Override
+                public KeyPhrase apply(DBObject input) {
+                    return keyPhraseTranslator.fromDBObject(input);
+                }
+            }));
+        }
+    }
+
+	@SuppressWarnings("unchecked")
+    private void decodeClips(DBObject dbObject, Content entity) {
+        if (dbObject.containsField(CLIPS_KEY)) {
 			Iterable<DBObject> clipsDbos = (Iterable<DBObject>) dbObject.get(CLIPS_KEY);
 			List<Clip> clips = Lists.newArrayList();
 			for (DBObject dbo : clipsDbos) {
@@ -44,20 +67,34 @@ public class ContentTranslator implements ModelTranslator<Content> {
             }
 			entity.setClips(clips);
 		}
-
-		return entity;
-	}
+    }
 
 	@Override
 	public DBObject toDBObject(DBObject dbObject, Content entity) {
 		dbObject = describedTranslator.toDBObject(dbObject, entity);
-		if (!entity.getClips().isEmpty()) {
+		encodeClips(dbObject, entity);
+        encodeKeyPhrases(dbObject, entity);
+        return dbObject;
+	}
+
+    private void encodeClips(DBObject dbObject, Content entity) {
+        if (!entity.getClips().isEmpty()) {
 		    BasicDBList clipDbos = new BasicDBList();
 		    for (Clip clip : entity.getClips()) {
 		        clipDbos.add(clipTranslator.toDBObject(new BasicDBObject(), clip));
             }
 			dbObject.put(CLIPS_KEY, clipDbos);
 		}
-        return dbObject;
-	}
+    }
+
+    private void encodeKeyPhrases(DBObject dbObject, Content entity) {
+        if (!entity.getKeyPhrases().isEmpty()) {
+            dbObject.put(PHRASES_KEY, ImmutableSet.copyOf(Iterables.transform(entity.getKeyPhrases(), new Function<KeyPhrase, DBObject>() {
+                @Override
+                public DBObject apply(KeyPhrase input) {
+                    return keyPhraseTranslator.toDBObject(input);
+                }
+            })));
+        }
+    }
 }
