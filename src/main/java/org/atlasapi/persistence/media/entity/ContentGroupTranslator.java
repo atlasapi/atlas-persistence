297f7ab4f6472e486eaa5f1e3ad0266d659be23d
Sergio Bossa <sergio.bossa@gmail.com>
2012-03-15 16:33:04 +0000

    ContentGroup ,mongo persistence implementation.

--- src/main/java/org/atlasapi/persistence/media/entity/ContentGroupTranslator.java
+++ src/main/java/org/atlasapi/persistence/media/entity/ContentGroupTranslator.java
@@ -9,32 +9,42 @@ import com.mongodb.DBObject;
 
 public class ContentGroupTranslator implements ModelTranslator<ContentGroup> {
 
-	public static final String CONTENT_URIS_KEY = "childRefs";
-	
-	private final DescribedTranslator contentTranslator;
-	private final ChildRefTranslator childTranslator;
+    public static final String GROUP_TYPE_KEY = "groupType";
+    public static final String CONTENT_URIS_KEY = "childRefs";
+    private final DescribedTranslator contentTranslator;
+    private final ChildRefTranslator childTranslator;
 
-	public ContentGroupTranslator() {
-		this(new DescribedTranslator(new IdentifiedTranslator()));
-	}
-	
-	public ContentGroupTranslator(DescribedTranslator contentTranslator) {
-		this.contentTranslator = contentTranslator;
+    public ContentGroupTranslator() {
+        this.contentTranslator = new DescribedTranslator(new IdentifiedTranslator(true));
         this.childTranslator = new ChildRefTranslator();
-	}
-	
+    }
+
     @Override
     public ContentGroup fromDBObject(DBObject dbObject, ContentGroup entity) {
         contentTranslator.fromDBObject(dbObject, entity);
+        entity.setType(ContentGroup.Type.valueOf(TranslatorUtils.toString(dbObject, GROUP_TYPE_KEY)));
         entity.setContents(childTranslator.fromDBObjects(TranslatorUtils.toDBObjectList(dbObject, CONTENT_URIS_KEY)));
+        entity.setReadHash(generateHashByRemovingFieldsFromTheDbo(dbObject));
         return entity;
     }
 
     @Override
     public DBObject toDBObject(DBObject dbObject, ContentGroup entity) {
-    	dbObject = contentTranslator.toDBObject(dbObject, entity);
-    	TranslatorUtils.from(dbObject, CONTENT_URIS_KEY, childTranslator.toDBList(entity.getContents()));
-    	dbObject.put(DescribedTranslator.TYPE_KEY, EntityType.from(entity));
-    	return dbObject;
+        dbObject = contentTranslator.toDBObject(dbObject, entity);
+        dbObject.put(DescribedTranslator.TYPE_KEY, EntityType.from(entity).toString());
+        TranslatorUtils.from(dbObject, GROUP_TYPE_KEY, entity.getType().toString());
+        TranslatorUtils.from(dbObject, CONTENT_URIS_KEY, childTranslator.toDBList(entity.getContents()));
+        return dbObject;
+    }
+
+    public String hashCodeOf(ContentGroup contentGroup) {
+        return generateHashByRemovingFieldsFromTheDbo(toDBObject(null, contentGroup));
+    }
+
+    private String generateHashByRemovingFieldsFromTheDbo(DBObject dbObject) {
+        dbObject.removeField(DescribedTranslator.LAST_FETCHED_KEY);
+        dbObject.removeField(DescribedTranslator.THIS_OR_CHILD_LAST_UPDATED_KEY);
+        dbObject.removeField(IdentifiedTranslator.LAST_UPDATED);
+        return String.valueOf(dbObject.hashCode());
     }
 }
