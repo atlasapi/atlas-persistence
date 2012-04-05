29a44bb4fc45dbdc1fc6d7c60ceed5583a9bef93
Daniel Bell <shinyhappydan@gmail.com>
2012-04-05 20:04:38 +0100

    Add availableOn to ChannelTranslator

--- src/main/java/org/atlasapi/media/channel/MongoChannelStore.java
+++ src/main/java/org/atlasapi/media/channel/MongoChannelStore.java
@@ -7,7 +7,6 @@ import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 
 import org.atlasapi.persistence.ids.MongoSequentialIdGenerator;
-import org.atlasapi.persistence.media.entity.IdentifiedTranslator;
 import org.joda.time.Duration;
 
 import com.google.common.base.Function;
