7fde211a0b4d65c52ec2020d3588feb4149bba01
Sergio Bossa <sergio.bossa@gmail.com>
2012-05-17 16:24:59 +0100

    Implemented cassandra content lister.

--- src/main/java/org/atlasapi/persistence/content/cassandra/CassandraContentStore.java
+++ src/main/java/org/atlasapi/persistence/content/cassandra/CassandraContentStore.java
@@ -6,9 +6,11 @@ import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.google.common.base.Function;
 import com.google.common.base.Joiner;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Iterators;
 import com.metabroadcast.common.base.Maybe;
 import com.netflix.astyanax.AstyanaxContext;
 import com.netflix.astyanax.Keyspace;
@@ -20,8 +22,12 @@ import com.netflix.astyanax.connectionpool.impl.CountingConnectionPoolMonitor;
 import com.netflix.astyanax.impl.AstyanaxConfigurationImpl;
 import com.netflix.astyanax.model.ColumnList;
 import com.netflix.astyanax.model.ConsistencyLevel;
+import com.netflix.astyanax.model.Row;
+import com.netflix.astyanax.model.Rows;
 import com.netflix.astyanax.thrift.ThriftFamilyFactory;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -43,12 +49,15 @@ import org.atlasapi.persistence.content.ContentWriter;
 import org.atlasapi.persistence.content.ResolvedContent;
 import org.atlasapi.serialization.json.JsonFactory;
 import static org.atlasapi.persistence.cassandra.CassandraSchema.*;
+import org.atlasapi.persistence.content.ContentCategory;
+import org.atlasapi.persistence.content.listing.ContentLister;
+import org.atlasapi.persistence.content.listing.ContentListingCriteria;
 import org.atlasapi.serialization.json.ContainerConfiguration;
 import org.atlasapi.serialization.json.ItemConfiguration;
 
 /**
  */
-public class CassandraContentStore implements ContentWriter, ContentResolver {
+public class CassandraContentStore implements ContentWriter, ContentResolver, ContentLister {
 
     private final ObjectMapper mapper = JsonFactory.makeJsonMapper();
     //
@@ -118,6 +127,47 @@ public class CassandraContentStore implements ContentWriter, ContentResolver {
             throw new CassandraPersistenceException(ex.getMessage(), ex);
         }
     }
+    
+    @Override
+    public Iterator<Content> listContent(ContentListingCriteria criteria) {
+        try {
+            Iterator<Content> items = Iterators.emptyIterator();
+            Iterator<Content> containers = Iterators.emptyIterator();
+            if (criteria.getCategories().contains(ContentCategory.CHILD_ITEM) || criteria.getCategories().contains(ContentCategory.TOP_LEVEL_ITEM)) {
+             OperationResult<Rows<String, String>> result = keyspace.prepareQuery(ITEMS_CF).getAllRows().execute();
+             items = Iterators.transform(result.getResult().iterator(), new Function<Row, Content>() {
+
+                    @Override
+                    public Content apply(Row input) {
+                        try {
+                        return unmarshalItem(input.getColumns());
+                        } catch(Exception ex) {
+                            return null;
+                        }
+                    }
+                 
+             });
+            }
+            if (criteria.getCategories().contains(ContentCategory.CONTAINER)) {
+             OperationResult<Rows<String, String>> result = keyspace.prepareQuery(CONTAINER_CF).getAllRows().execute();
+             containers = Iterators.transform(result.getResult().iterator(), new Function<Row, Content>() {
+
+                    @Override
+                    public Content apply(Row input) {
+                        try {
+                        return unmarshalContainer(input.getColumns());
+                        } catch(Exception ex) {
+                            return null;
+                        }
+                    }
+                 
+             });
+            }
+            return Iterators.concat(containers, items);
+        } catch (Exception ex) {
+            throw new CassandraPersistenceException(ex.getMessage(), ex);
+        }
+    }
 
     private void writeItem(Item item) throws Exception {
         MutationBatch mutation = keyspace.prepareMutationBatch();
@@ -181,12 +231,7 @@ public class CassandraContentStore implements ContentWriter, ContentResolver {
             Future<OperationResult<ColumnList<String>>> result = keyspace.prepareQuery(ITEMS_CF).getKey(id.toString()).executeAsync();
             OperationResult<ColumnList<String>> columns = result.get(requestTimeout, TimeUnit.MILLISECONDS);
             if (!columns.getResult().isEmpty()) {
-                Item item = mapper.readValue(columns.getResult().getColumnByName(ITEM_COLUMN).getByteArrayValue(), Item.class);
-                List<Clip> clips = mapper.readValue(columns.getResult().getColumnByName(CLIPS_COLUMN).getByteArrayValue(), TypeFactory.defaultInstance().constructCollectionType(List.class, Clip.class));
-                Set<Version> versions = mapper.readValue(columns.getResult().getColumnByName(VERSIONS_COLUMN).getByteArrayValue(), TypeFactory.defaultInstance().constructCollectionType(Set.class, Version.class));
-                item.setClips(clips);
-                item.setVersions(versions);
-                return item;
+                return unmarshalItem(columns.getResult());
             } else {
                 return null;
             }
@@ -200,17 +245,30 @@ public class CassandraContentStore implements ContentWriter, ContentResolver {
             Future<OperationResult<ColumnList<String>>> result = keyspace.prepareQuery(CONTAINER_CF).getKey(id.toString()).executeAsync();
             OperationResult<ColumnList<String>> columns = result.get(requestTimeout, TimeUnit.MILLISECONDS);
             if (!columns.getResult().isEmpty()) {
-                Container container = mapper.readValue(columns.getResult().getColumnByName(ITEM_COLUMN).getByteArrayValue(), Container.class);
-                List<Clip> clips = mapper.readValue(columns.getResult().getColumnByName(CLIPS_COLUMN).getByteArrayValue(), TypeFactory.defaultInstance().constructCollectionType(List.class, Clip.class));
-                List<ChildRef> children = mapper.readValue(columns.getResult().getColumnByName(CHILDREN_COLUMN).getByteArrayValue(), TypeFactory.defaultInstance().constructCollectionType(List.class, ChildRef.class));
-                container.setClips(clips);
-                container.setChildRefs(children);
-                return container;
+                return unmarshalContainer(columns.getResult());
             } else {
                 return null;
             }
         } catch (Exception ex) {
             throw new CassandraPersistenceException(ex.getMessage(), ex);
         }
+    }
+    
+    private Content unmarshalItem(ColumnList<String> columns) throws IOException {
+        Item item = mapper.readValue(columns.getColumnByName(ITEM_COLUMN).getByteArrayValue(), Item.class);
+        List<Clip> clips = mapper.readValue(columns.getColumnByName(CLIPS_COLUMN).getByteArrayValue(), TypeFactory.defaultInstance().constructCollectionType(List.class, Clip.class));
+        Set<Version> versions = mapper.readValue(columns.getColumnByName(VERSIONS_COLUMN).getByteArrayValue(), TypeFactory.defaultInstance().constructCollectionType(Set.class, Version.class));
+        item.setClips(clips);
+        item.setVersions(versions);
+        return item;
+    }
+    
+    private Container unmarshalContainer(ColumnList<String> columns) throws IOException {
+        Container container = mapper.readValue(columns.getColumnByName(ITEM_COLUMN).getByteArrayValue(), Container.class);
+        List<Clip> clips = mapper.readValue(columns.getColumnByName(CLIPS_COLUMN).getByteArrayValue(), TypeFactory.defaultInstance().constructCollectionType(List.class, Clip.class));
+        List<ChildRef> children = mapper.readValue(columns.getColumnByName(CHILDREN_COLUMN).getByteArrayValue(), TypeFactory.defaultInstance().constructCollectionType(List.class, ChildRef.class));
+        container.setClips(clips);
+        container.setChildRefs(children);
+        return container;
     }
 }
