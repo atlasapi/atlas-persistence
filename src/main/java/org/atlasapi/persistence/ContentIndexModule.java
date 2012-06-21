210a73ffd313bbbabcf3d0b3768c77ab9f13b3ed
Sergio Bossa <sergio.bossa@gmail.com>
2012-06-21 19:52:32 +0100

    Added ES index init.

--- src/main/java/org/atlasapi/persistence/ContentIndexModule.java
+++ src/main/java/org/atlasapi/persistence/ContentIndexModule.java
@@ -6,5 +6,7 @@ import org.atlasapi.persistence.content.ContentIndexer;
  */
 public interface ContentIndexModule {
     
+    public void init();
+    
     public ContentIndexer contentIndexer();
 }
