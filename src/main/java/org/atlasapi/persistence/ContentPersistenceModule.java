e146325f66fcfa74c0dab4b3b3c28599f38adf53
Sergio Bossa <sergio.bossa@gmail.com>
2012-08-06 13:11:21 +0100

    Fixed Spring dependencies.

--- src/main/java/org/atlasapi/persistence/ContentPersistenceModule.java
+++ src/main/java/org/atlasapi/persistence/ContentPersistenceModule.java
@@ -11,13 +11,14 @@ import org.atlasapi.persistence.content.ContentResolver;
 import org.atlasapi.persistence.content.ContentWriter;
 import org.atlasapi.persistence.content.KnownTypeContentResolver;
 import org.atlasapi.persistence.content.ScheduleResolver;
+import org.atlasapi.persistence.content.listing.ContentLister;
 import org.atlasapi.persistence.content.mongo.LastUpdatedContentFinder;
 import org.atlasapi.persistence.content.people.ItemsPeopleWriter;
 import org.atlasapi.persistence.content.schedule.mongo.ScheduleWriter;
 import org.atlasapi.persistence.event.RecentChangeStore;
 import org.atlasapi.persistence.lookup.entry.LookupEntryStore;
 import org.atlasapi.persistence.shorturls.ShortUrlSaver;
-import org.atlasapi.persistence.topic.TopicContentLister;
+import org.atlasapi.persistence.topic.TopicContentUriLister;
 import org.atlasapi.persistence.topic.TopicQueryResolver;
 import org.atlasapi.persistence.topic.TopicStore;
 
@@ -62,4 +63,8 @@ public interface ContentPersistenceModule {
 	TopicContentLister topicContentLister();
 	
     RecentChangeStore recentChangesStore();
+    
+    ContentLister contentLister();
+        
+    TopicContentUriLister topicContentUriLister();
 }
