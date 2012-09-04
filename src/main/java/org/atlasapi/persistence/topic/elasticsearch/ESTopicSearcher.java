7401bc42082f5a0b5235b75b4dcfaaf0f123e28c
Sergio Bossa <sergio.bossa@gmail.com>
2012-09-04 13:15:10 +0100

    Added pagination to elasticsearch-based topic searcher.

--- src/main/java/org/atlasapi/persistence/topic/elasticsearch/ESTopicSearcher.java
+++ src/main/java/org/atlasapi/persistence/topic/elasticsearch/ESTopicSearcher.java
@@ -1,6 +1,8 @@
 package org.atlasapi.persistence.topic.elasticsearch;
 
+import com.google.common.collect.Iterables;
 import com.metabroadcast.common.base.Maybe;
+import com.metabroadcast.common.query.Selection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -33,11 +35,11 @@ public class ESTopicSearcher implements TopicSearcher {
     }
     
     @Override
-    public List<Topic> popularTopics(Interval interval, TopicQueryResolver resolver) {
-        Facets facets = buildQuery(interval).actionGet(timeout, TimeUnit.MILLISECONDS).getFacets();
+    public List<Topic> popularTopics(Interval interval, TopicQueryResolver resolver, Selection selection) {
+        Facets facets = buildQuery(interval, selection).actionGet(timeout, TimeUnit.MILLISECONDS).getFacets();
         TermsFacet terms = facets.facet(TermsFacet.class, ESItem.TOPICS);
         List<Topic> result = new LinkedList<Topic>();
-        for (TermsFacet.Entry term : terms.entries()) {
+        for (TermsFacet.Entry term : Iterables.limit(Iterables.skip(terms.entries(), selection.getOffset()), selection.getLimit())) {
             Maybe<Topic> topic = resolver.topicForId(Long.parseLong(term.getTerm()));
             if (topic.hasValue()) {
                 result.add(topic.requireValue());
@@ -46,10 +48,13 @@ public class ESTopicSearcher implements TopicSearcher {
         return result;
     }
 
-    private ListenableActionFuture<SearchResponse> buildQuery(Interval interval) {
+    private ListenableActionFuture<SearchResponse> buildQuery(Interval interval, Selection selection) {
+        System.out.println(index.client().prepareSearch(ESSchema.INDEX_NAME).setQuery(
+                QueryBuilders.nestedQuery(ESItem.BROADCASTS, QueryBuilders.rangeQuery(ESBroadcast.TRANSMISSION_TIME).from(interval.getStart()).to(interval.getEnd()))).
+                addFacet(FacetBuilders.termsFacet(ESItem.TOPICS).field(ESItem.TOPICS + "." + ESTopic.ID).size(selection.getOffset() + selection.getLimit())).toString());
         ListenableActionFuture<SearchResponse> result = index.client().prepareSearch(ESSchema.INDEX_NAME).setQuery(
                 QueryBuilders.nestedQuery(ESItem.BROADCASTS, QueryBuilders.rangeQuery(ESBroadcast.TRANSMISSION_TIME).from(interval.getStart()).to(interval.getEnd()))).
-                addFacet(FacetBuilders.termsFacet(ESItem.TOPICS).field(ESItem.TOPICS + "." + ESTopic.ID)).
+                addFacet(FacetBuilders.termsFacet(ESItem.TOPICS).field(ESItem.TOPICS + "." + ESTopic.ID).size(selection.getOffset() + selection.getLimit())).
                 execute();
         return result;
     }
