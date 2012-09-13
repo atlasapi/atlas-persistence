68319b804c323a8544aca331a2fcb69229af5e69
Sergio Bossa <sergio.bossa@gmail.com>
2012-09-13 17:55:47 +0100

    Using configurer for cassandra properties.

--- src/main/java/org/atlasapi/persistence/CassandraPersistenceModule.java
+++ src/main/java/org/atlasapi/persistence/CassandraPersistenceModule.java
@@ -2,9 +2,9 @@ package org.atlasapi.persistence;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
+import com.metabroadcast.common.properties.Configurer;
 import org.atlasapi.persistence.content.cassandra.CassandraContentStore;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,14 +12,16 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class CassandraPersistenceModule {
-    
-    private @Value("${cassandra.env}") String environment;
-    private @Value("${cassandra.seeds}") String seeds;
-    private @Value("${cassandra.port}") int port;
-    private @Value("${cassandra.connectionTimeout}") int connectionTimeout;
-    private @Value("${cassandra.requestTimeout}") int requestTimeout;
-    
-    public @Bean @Qualifier(value="cassandra") CassandraContentStore cassandraContentStore() {
-        return new CassandraContentStore(environment, Lists.newArrayList(Splitter.on(',').split(seeds)), port, Runtime.getRuntime().availableProcessors() * 10, connectionTimeout, requestTimeout);
+
+    private final String environment = Configurer.get("cassandra.env").get();
+    private final String seeds = Configurer.get("cassandra.seeds").get();
+    private final String port = Configurer.get("cassandra.port").get();
+    private final String connectionTimeout = Configurer.get("cassandra.connectionTimeout").get();
+    private final String requestTimeout = Configurer.get("cassandra.requestTimeout").get();
+
+    @Bean
+    @Qualifier(value = "cassandra")
+    public CassandraContentStore cassandraContentStore() {
+        return new CassandraContentStore(environment, Lists.newArrayList(Splitter.on(',').split(seeds)), Integer.parseInt(port), Runtime.getRuntime().availableProcessors() * 10, Integer.parseInt(connectionTimeout), Integer.parseInt(requestTimeout));
     }
 }
