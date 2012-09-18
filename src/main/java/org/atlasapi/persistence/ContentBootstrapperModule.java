4396cf9538768d3e4ef6f6c156fc5487c27399c4
Sergio Bossa <sergio.bossa@gmail.com>
2012-09-18 10:33:46 +0100

    Now bootstrapping content from Mongo too.

--- src/main/java/org/atlasapi/persistence/ContentBootstrapperModule.java
+++ src/main/java/org/atlasapi/persistence/ContentBootstrapperModule.java
@@ -2,17 +2,20 @@ package org.atlasapi.persistence;
 
 import org.atlasapi.persistence.bootstrap.ContentBootstrapper;
 import org.atlasapi.persistence.content.cassandra.CassandraContentStore;
+import org.atlasapi.persistence.content.mongo.MongoContentLister;
 
 /**
  */
 public class ContentBootstrapperModule {
 
+    private final MongoContentLister mongoContentStore;
     private final CassandraContentStore cassandraContentStore;
     private final ContentBootstrapper esContentBootstrapper;
 
-    public ContentBootstrapperModule(CassandraContentStore cassandraContentStore) {
+    public ContentBootstrapperModule(MongoContentLister mongoContentStore, CassandraContentStore cassandraContentStore) {
+        this.mongoContentStore = mongoContentStore;
         this.cassandraContentStore = cassandraContentStore;
-        this.esContentBootstrapper = new ContentBootstrapper().withContentListers(cassandraContentStore);
+        this.esContentBootstrapper = new ContentBootstrapper().withContentListers(mongoContentStore, cassandraContentStore);
     }
 
     public ContentBootstrapper contentBootstrapper() {
