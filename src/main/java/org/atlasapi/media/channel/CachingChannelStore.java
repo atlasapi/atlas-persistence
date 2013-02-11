baecce803a427a909b73b51486eb0e7b854de416
Oli Hall <oli@metabradcast.com>
2013-02-11 18:30:29 +0000

    Change channel cache to be BackgroundComputingValue instead of LoadingCache

--- src/main/java/org/atlasapi/media/channel/CachingChannelStore.java
+++ src/main/java/org/atlasapi/media/channel/CachingChannelStore.java
@@ -1,60 +1,42 @@
 package org.atlasapi.media.channel;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
+import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 
-import com.google.common.base.Function;
+import org.joda.time.Duration;
+
+import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
-import com.google.common.base.Throwables;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableMap.Builder;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Maps;
 import com.metabroadcast.common.base.Maybe;
+import com.metabroadcast.common.caching.BackgroundComputingValue;
 
 public class CachingChannelStore implements ChannelStore {
 
     private final ChannelStore delegate;
-       
-    private final LoadingCache<String, Maybe<Channel>> channelUriCache = CacheBuilder.newBuilder()
-        .expireAfterWrite(5, TimeUnit.MINUTES)
-        .build(new CacheLoader<String, Maybe<Channel>>() {
-            @Override
-            public Maybe<Channel> load(String uri) throws Exception {
-                Maybe<Channel> result = delegate.fromUri(uri);
-                return result;
-            }
-        });
+    private final BackgroundComputingValue<List<Channel>> channels;
     
     public CachingChannelStore(ChannelStore delegate) {
         this.delegate = delegate;
-        for (Channel channel : delegate.all()) {
-            channelUriCache.put(channel.uri(), Maybe.just(channel));
-        }
+        channels = new BackgroundComputingValue<List<Channel>>(Duration.standardMinutes(5), new ChannelsUpdater(delegate));
     }
 
     @Override
     public Channel createOrUpdate(Channel channel) {
-        Channel written = delegate.createOrUpdate(channel);
-        checkNotNull(written.uri(), "uri null for channel " + channel.title());
-        channelUriCache.put(written.uri(), Maybe.just(written));
-        return written;
+        return delegate.createOrUpdate(channel);
     }
 
     @Override
     @Deprecated
     public Maybe<Channel> fromKey(String key) {
-        for (Maybe<Channel> channel : channelUriCache.asMap().values()) {
-            if (channel.hasValue()) {
-                if (channel.requireValue().key().equals(key)) { 
-                    return channel;
-                }
+        for (Channel channel : channels.get()) {
+            if (channel.key().equals(key)) { 
+                return Maybe.just(channel);
             }
         }
         return Maybe.nothing();
@@ -62,11 +44,9 @@ public class CachingChannelStore implements ChannelStore {
 
     @Override
     public Maybe<Channel> fromId(long id) {
-        for (Maybe<Channel> channel : channelUriCache.asMap().values()) {
-            if (channel.hasValue()) {
-                if (channel.requireValue().getId().equals(id)) { 
-                    return channel;
-                }
+        for (Channel channel : channels.get()) {
+            if (channel.getId().equals(id)) { 
+                return Maybe.just(channel);
             }
         }
         return Maybe.nothing();
@@ -74,58 +54,34 @@ public class CachingChannelStore implements ChannelStore {
 
     @Override
     public Maybe<Channel> fromUri(String uri) {
-        try {
-            return channelUriCache.get(uri);
-        } catch (ExecutionException e) {
-            Throwables.propagate(e);
-            // will never reach here
-            return null;
+        for (Channel channel : channels.get()) {
+            if (channel.uri().equals(uri)) { 
+                return Maybe.just(channel);
+            }
         }
+        return Maybe.nothing();
     }
 
     @Override
     public Iterable<Channel> forIds(final Iterable<Long> ids) {
-        return Iterables.filter(
-            Iterables.transform(
-            channelUriCache.asMap().values(), 
-                new Function<Maybe<Channel>, Channel>() {
-                    @Override
-                    public Channel apply(Maybe<Channel> input) {
-                        if (input.hasValue() && Iterables.contains(ids, input.requireValue().getId())) {
-                            return input.requireValue();
-                        }
-                        return null;
-                    }
+        return Iterables.filter(channels.get(), new Predicate<Channel>() {
+                @Override
+                public boolean apply(Channel input) {
+                    return Iterables.contains(ids, input.getId());
                 }
-            ), Predicates.notNull());
+            });
     }
 
     @Override
     public Iterable<Channel> all() {
-        return Iterables.filter(
-            Iterables.transform(
-            channelUriCache.asMap().values(), 
-                new Function<Maybe<Channel>, Channel>() {
-                    @Override
-                    public Channel apply(Maybe<Channel> input) {
-                        if (input.hasValue()) {
-                            return input.requireValue();
-                        }
-                        return null;
-                    }
-                }
-            ), Predicates.notNull());
+        return channels.get();
     }
 
     @Override
     public Maybe<Channel> forAlias(String alias) {
-        for (Maybe<Channel> channel : channelUriCache.asMap().values()) {
-            if (channel.hasValue()) {
-                for (String channelAlias : channel.requireValue().getAliases()) {
-                    if (alias.equals(channelAlias)) {
-                        return channel;
-                    }
-                }
+        for (Channel channel : channels.get()) {
+            if (channel.getAliases().contains(alias)) { 
+                return Maybe.just(channel);
             }
         }
         return Maybe.nothing();
@@ -135,15 +91,26 @@ public class CachingChannelStore implements ChannelStore {
     public Map<String, Channel> forAliases(String aliasPrefix) {
         final Pattern prefixPattern = Pattern.compile(String.format("^%s", Pattern.quote(aliasPrefix)));
 
-        Builder<String, Channel> channelMap = ImmutableMap.builder();
-        for (Maybe<Channel> channel : channelUriCache.asMap().values()) {
-            if (channel.hasValue()) {
-                for (String alias : Iterables.filter(channel.requireValue().getAliases(), Predicates.contains(prefixPattern))) {
-                    channelMap.put(alias, channel.requireValue());
-                }
+        Map<String, Channel> channelMap = Maps.newHashMap();
+        for (Channel channel : channels.get()) {
+            for (String alias : Iterables.filter(channel.getAliases(), Predicates.contains(prefixPattern))) {
+                channelMap.put(alias, channel);
             }
         }
-        return channelMap.build();
+        return ImmutableMap.copyOf(channelMap);
     }
 
+    private static class ChannelsUpdater implements Callable<List<Channel>> {
+        private final ChannelResolver delegate;
+        
+        public ChannelsUpdater(ChannelResolver delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public List<Channel> call() throws Exception {
+            return ImmutableList.copyOf(delegate.all());
+        }
+        
+    }
 }
