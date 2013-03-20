28571ca095d38c40f25dd4ba653f76ff1dd9622d
Oli Hall <oli@metabradcast.com>
2013-03-20 10:34:33 +0000

    Persist the activelyPublished flag

--- src/main/java/org/atlasapi/persistence/media/entity/DescribedTranslator.java
+++ src/main/java/org/atlasapi/persistence/media/entity/DescribedTranslator.java
@@ -27,6 +27,17 @@ import com.mongodb.DBObject;
 
 public class DescribedTranslator implements ModelTranslator<Described> {
 
+    private static final String TAGS_KEY = "tags";
+    private static final String THUMBNAIL_KEY = "thumbnail";
+    private static final String TITLE_KEY = "title";
+    private static final String MEDIA_TYPE_KEY = "mediaType";
+    private static final String SPECIALIZATION_KEY = "specialization";
+    private static final String PRESENTATION_CHANNEL_KEY = "presentationChannel";
+    private static final String IMAGES_KEY = "images";
+    private static final String IMAGE_KEY = "image";
+    private static final String GENRES_KEY = "genres";
+    private static final String FIRST_SEEN_KEY = "firstSeen";
+    private static final String DESCRIPTION_KEY = "description";
     public static final String PUBLISHER_KEY = "publisher";
     private static final String SCHEDULE_ONLY_KEY = "scheduleOnly";
     public static final String THIS_OR_CHILD_LAST_UPDATED_KEY = "thisOrChildLastUpdated";
@@ -35,6 +46,7 @@ public class DescribedTranslator implements ModelTranslator<Described> {
     public static final String SHORT_DESC_KEY = "shortDescription";
     public static final String MEDIUM_DESC_KEY = "mediumDescription";
     public static final String LONG_DESC_KEY = "longDescription";
+    private static final String ACTIVELY_PUBLISHED_KEY = "activelyPublished";
 	
 	private final IdentifiedTranslator descriptionTranslator;
     private final ImageTranslator imageTranslator;
@@ -49,17 +61,17 @@ public class DescribedTranslator implements ModelTranslator<Described> {
 
 		descriptionTranslator.fromDBObject(dbObject, entity);
 
-		entity.setDescription((String) dbObject.get("description"));
+		entity.setDescription((String) dbObject.get(DESCRIPTION_KEY));
 		
 		entity.setShortDescription(TranslatorUtils.toString(dbObject, SHORT_DESC_KEY));
 		entity.setMediumDescription(TranslatorUtils.toString(dbObject, MEDIUM_DESC_KEY));
 		entity.setLongDescription(TranslatorUtils.toString(dbObject, LONG_DESC_KEY));
 
-		entity.setFirstSeen(TranslatorUtils.toDateTime(dbObject, "firstSeen"));
+		entity.setFirstSeen(TranslatorUtils.toDateTime(dbObject, FIRST_SEEN_KEY));
 		entity.setThisOrChildLastUpdated(TranslatorUtils.toDateTime(dbObject, THIS_OR_CHILD_LAST_UPDATED_KEY));
 
-		entity.setGenres(TranslatorUtils.toSet(dbObject, "genres"));
-		entity.setImage((String) dbObject.get("image"));
+		entity.setGenres(TranslatorUtils.toSet(dbObject, GENRES_KEY));
+		entity.setImage((String) dbObject.get(IMAGE_KEY));
 		entity.setLastFetched(TranslatorUtils.toDateTime(dbObject, LAST_FETCHED_KEY));
 		Boolean scheduleOnly = TranslatorUtils.toBoolean(dbObject, SCHEDULE_ONLY_KEY);
 		entity.setScheduleOnly(scheduleOnly != null ? scheduleOnly : false);
@@ -69,7 +81,7 @@ public class DescribedTranslator implements ModelTranslator<Described> {
 			entity.setPublisher(Publisher.fromKey(publisherKey).valueOrDefault(null));
 		}
 		
-		List<DBObject> imagesDboList = TranslatorUtils.toDBObjectList(dbObject, "images");
+		List<DBObject> imagesDboList = TranslatorUtils.toDBObjectList(dbObject, IMAGES_KEY);
 		
 		Set<Image> images = Sets.newHashSet();
 		if (imagesDboList != null) {
@@ -79,21 +91,25 @@ public class DescribedTranslator implements ModelTranslator<Described> {
 		}
 		entity.setImages(images);
 
-		entity.setTags(TranslatorUtils.toSet(dbObject, "tags"));
-		entity.setThumbnail((String) dbObject.get("thumbnail"));
-		entity.setTitle((String) dbObject.get("title"));
+		entity.setTags(TranslatorUtils.toSet(dbObject, TAGS_KEY));
+		entity.setThumbnail((String) dbObject.get(THUMBNAIL_KEY));
+		entity.setTitle((String) dbObject.get(TITLE_KEY));
 
-		String cType = (String) dbObject.get("mediaType");
+		String cType = (String) dbObject.get(MEDIA_TYPE_KEY);
 		if (cType != null) {
 			entity.setMediaType(MediaType.valueOf(cType.toUpperCase()));
 		}
 
-		String specialization = (String) dbObject.get("specialization");
+		String specialization = (String) dbObject.get(SPECIALIZATION_KEY);
 		if (specialization != null) {
 			entity.setSpecialization(Specialization.valueOf(specialization.toUpperCase()));
 		}
 		
-		entity.setPresentationChannel(TranslatorUtils.toString(dbObject, "presentationChannel"));
+		entity.setPresentationChannel(TranslatorUtils.toString(dbObject, PRESENTATION_CHANNEL_KEY));
+		
+		if (dbObject.containsField(ACTIVELY_PUBLISHED_KEY)) {
+		    entity.setActivelyPublished(TranslatorUtils.toBoolean(dbObject, ACTIVELY_PUBLISHED_KEY));
+		}
 
 		return entity;
 	}
@@ -110,30 +126,30 @@ public class DescribedTranslator implements ModelTranslator<Described> {
 	    TranslatorUtils.from(dbObject, MEDIUM_DESC_KEY, entity.getMediumDescription());
 	    TranslatorUtils.from(dbObject, LONG_DESC_KEY, entity.getLongDescription());
 
-        TranslatorUtils.from(dbObject, "description", entity.getDescription());
-        TranslatorUtils.fromDateTime(dbObject, "firstSeen", entity.getFirstSeen());
+        TranslatorUtils.from(dbObject, DESCRIPTION_KEY, entity.getDescription());
+        TranslatorUtils.fromDateTime(dbObject, FIRST_SEEN_KEY, entity.getFirstSeen());
         TranslatorUtils.fromDateTime(dbObject, THIS_OR_CHILD_LAST_UPDATED_KEY, entity.getThisOrChildLastUpdated());
-        TranslatorUtils.fromSet(dbObject, entity.getGenres(), "genres");
-        TranslatorUtils.from(dbObject, "image", entity.getImage());
+        TranslatorUtils.fromSet(dbObject, entity.getGenres(), GENRES_KEY);
+        TranslatorUtils.from(dbObject, IMAGE_KEY, entity.getImage());
         TranslatorUtils.fromDateTime(dbObject, LAST_FETCHED_KEY, entity.getLastFetched());
         
         if (entity.getPublisher() != null) {
         	TranslatorUtils.from(dbObject, PUBLISHER_KEY, entity.getPublisher().key());
         }
         
-        TranslatorUtils.fromSet(dbObject, entity.getTags(), "tags");
-        TranslatorUtils.from(dbObject, "thumbnail", entity.getThumbnail());
-        TranslatorUtils.from(dbObject, "title", entity.getTitle());
+        TranslatorUtils.fromSet(dbObject, entity.getTags(), TAGS_KEY);
+        TranslatorUtils.from(dbObject, THUMBNAIL_KEY, entity.getThumbnail());
+        TranslatorUtils.from(dbObject, TITLE_KEY, entity.getTitle());
         
         if (entity.isScheduleOnly()) {
             dbObject.put(SCHEDULE_ONLY_KEY, Boolean.TRUE);
         }
         
         if (entity.getMediaType() != null) {
-        	TranslatorUtils.from(dbObject, "mediaType", entity.getMediaType().toString().toLowerCase());
+        	TranslatorUtils.from(dbObject, MEDIA_TYPE_KEY, entity.getMediaType().toString().toLowerCase());
         }
         if (entity.getSpecialization() != null) {
-            TranslatorUtils.from(dbObject, "specialization", entity.getSpecialization().toString().toLowerCase());
+            TranslatorUtils.from(dbObject, SPECIALIZATION_KEY, entity.getSpecialization().toString().toLowerCase());
         }
         
         BasicDBList images = new BasicDBList();
@@ -142,9 +158,10 @@ public class DescribedTranslator implements ModelTranslator<Described> {
                 images.add(imageTranslator.toDBObject(null, image));
             }
         }
-        dbObject.put("images", images);
+        dbObject.put(IMAGES_KEY, images);
         
-        TranslatorUtils.from(dbObject, "presentationChannel", entity.getPresentationChannel());
+        TranslatorUtils.from(dbObject, PRESENTATION_CHANNEL_KEY, entity.getPresentationChannel());
+        TranslatorUtils.from(dbObject, ACTIVELY_PUBLISHED_KEY, entity.isActivelyPublished());
         
         return dbObject;
 	}
