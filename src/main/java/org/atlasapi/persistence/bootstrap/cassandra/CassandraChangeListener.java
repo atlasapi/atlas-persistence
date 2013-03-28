ee8a16c236bca240360e036455f8ece38f4a2be3
Fred van den Driessche <fred@metabroadcast.com>
2013-03-28 17:08:49 +0000

    generalise change listeners

--- src/main/java/org/atlasapi/persistence/bootstrap/cassandra/CassandraChangeListener.java
+++ src/main/java/org/atlasapi/persistence/bootstrap/cassandra/CassandraChangeListener.java
@@ -4,12 +4,14 @@ import java.util.concurrent.ThreadPoolExecutor;
 import org.atlasapi.media.channel.Channel;
 import org.atlasapi.media.channel.ChannelGroup;
 import org.atlasapi.media.content.Container;
+import org.atlasapi.media.content.ContentStore;
 import org.atlasapi.media.entity.ContentGroup;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.media.entity.Person;
 import org.atlasapi.media.product.Product;
 import org.atlasapi.media.segment.Segment;
 import org.atlasapi.media.topic.Topic;
+import org.atlasapi.media.topic.TopicStore;
 import org.atlasapi.persistence.bootstrap.AbstractMultiThreadedChangeListener;
 import org.atlasapi.persistence.content.cassandra.CassandraContentGroupStore;
 import org.atlasapi.persistence.content.cassandra.CassandraContentStore;
@@ -26,7 +28,7 @@ import org.atlasapi.persistence.topic.cassandra.CassandraTopicStore;
  */
 public class CassandraChangeListener extends AbstractMultiThreadedChangeListener {
 
-    private CassandraContentStore cassandraContentStore;
+    private ContentStore cassandraContentStore;
     private CassandraChannelGroupStore cassandraChannelGroupStore;
     private CassandraChannelStore cassandraChannelStore;
     private CassandraContentGroupStore cassandraContentGroupStore;
@@ -34,7 +36,7 @@ public class CassandraChangeListener extends AbstractMultiThreadedChangeListener
     private CassandraPersonStore cassandraPersonStore;
     private CassandraProductStore cassandraProductStore;
     private CassandraSegmentStore cassandraSegmentStore;
-    private CassandraTopicStore cassandraTopicStore;
+    private TopicStore cassandraTopicStore;
 
     public CassandraChangeListener(int concurrencyLevel) {
         super(concurrencyLevel);
@@ -56,7 +58,7 @@ public class CassandraChangeListener extends AbstractMultiThreadedChangeListener
         this.cassandraContentGroupStore = cassandraContentGroupStore;
     }
 
-    public void setCassandraContentStore(CassandraContentStore cassandraContentStore) {
+    public void setCassandraContentStore(ContentStore cassandraContentStore) {
         this.cassandraContentStore = cassandraContentStore;
     }
 
@@ -72,7 +74,7 @@ public class CassandraChangeListener extends AbstractMultiThreadedChangeListener
         this.cassandraSegmentStore = cassandraSegmentStore;
     }
 
-    public void setCassandraTopicStore(CassandraTopicStore cassandraTopicStore) {
+    public void setCassandraTopicStore(TopicStore cassandraTopicStore) {
         this.cassandraTopicStore = cassandraTopicStore;
     }
 
@@ -85,13 +87,13 @@ public class CassandraChangeListener extends AbstractMultiThreadedChangeListener
         if (change instanceof Item) {
             Item item = (Item) change;
             item.setReadHash(null);
-            cassandraContentStore.createOrUpdate(item);
+            cassandraContentStore.writeContent(item);
         } else if (change instanceof LookupEntry) {
             cassandraLookupEntryStore.store((LookupEntry) change);
         } else if (change instanceof Container) {
             Container container = (Container) change;
             container.setReadHash(null);
-            cassandraContentStore.createOrUpdate(container);
+            cassandraContentStore.writeContent(container);
         } else if (change instanceof Person) {
             cassandraPersonStore.createOrUpdatePerson((Person) change);
         } else if (change instanceof Channel) {
@@ -101,7 +103,7 @@ public class CassandraChangeListener extends AbstractMultiThreadedChangeListener
         } else if (change instanceof Segment) {
             cassandraSegmentStore.write((Segment) change);
         } else if (change instanceof Topic) {
-            cassandraTopicStore.write((Topic) change);
+            cassandraTopicStore.writeTopic((Topic) change);
         } else if (change instanceof ContentGroup) {
             cassandraContentGroupStore.createOrUpdate((ContentGroup) change);
         } else if (change instanceof ChannelGroup) {
