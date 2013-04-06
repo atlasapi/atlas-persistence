df4246c475992ed5fe94183b059b674acb5f6ab5
Fred van den Driessche <fred@metabroadcast.com>
2013-04-06 19:25:17 +0100

    add resolveId methods to old ContentResolvers

--- src/main/java/org/atlasapi/persistence/content/LookupResolvingContentResolver.java
+++ src/main/java/org/atlasapi/persistence/content/LookupResolvingContentResolver.java
@@ -1,5 +1,8 @@
 package org.atlasapi.persistence.content;
 
+import java.util.Set;
+
+import org.atlasapi.media.common.Id;
 import org.atlasapi.media.entity.LookupRef;
 import org.atlasapi.persistence.lookup.entry.LookupEntry;
 import org.atlasapi.persistence.lookup.entry.LookupEntryStore;
@@ -7,6 +10,7 @@ import org.atlasapi.persistence.lookup.entry.LookupEntryStore;
 import com.google.common.base.Predicates;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Sets;
 
 public class LookupResolvingContentResolver implements ContentResolver {
 
@@ -20,10 +24,22 @@ public class LookupResolvingContentResolver implements ContentResolver {
     
     @Override
     public ResolvedContent findByCanonicalUris(Iterable<String> canonicalUris) {
-        Iterable<LookupRef> lookupRefs = Iterables.transform(lookupResolver.entriesForCanonicalUris(canonicalUris), LookupEntry.TO_SELF);
+        Set<String> dedupedUris = Sets.newHashSet(canonicalUris);
+        Iterable<LookupEntry> lookupEntries = lookupResolver.entriesForCanonicalUris(dedupedUris);
+        Iterable<LookupRef> lookupRefs = Iterables.transform(lookupEntries, LookupEntry.TO_SELF);
         ImmutableSet<LookupRef> resolvedLookups = ImmutableSet.copyOf(Iterables.filter(lookupRefs, Predicates.notNull()));
         ResolvedContent resolvedContent = knownTypeResolver.findByLookupRefs(resolvedLookups);
-        return resolvedContent.copyWithAllRequestedUris(canonicalUris);
+        return resolvedContent;//.copyWithAllRequestedUris(canonicalUris);
+    }
+
+    @Override
+    public ResolvedContent findByIds(Iterable<Id> ids) {
+        Set<Id> dedupedIds = Sets.newHashSet(ids);
+        Iterable<LookupEntry> lookupEntries = lookupResolver.entriesForIds(Iterables.transform(dedupedIds,Id.toLongValue()));
+        Iterable<LookupRef> lookupRefs = Iterables.transform(lookupEntries, LookupEntry.TO_SELF);
+        ImmutableSet<LookupRef> resolvedLookups = ImmutableSet.copyOf(Iterables.filter(lookupRefs, Predicates.notNull()));
+        ResolvedContent resolvedContent = knownTypeResolver.findByLookupRefs(resolvedLookups);
+        return resolvedContent;
     }
 }
 
