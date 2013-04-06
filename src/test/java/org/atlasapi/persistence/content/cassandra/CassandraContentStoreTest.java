010865e2565b8ef06e7e6007bfb5cdf060aaba36
Fred van den Driessche <fred@metabroadcast.com>
2013-04-06 19:03:50 +0100

    replace instantiations of Container with Brand

--- src/test/java/org/atlasapi/persistence/content/cassandra/CassandraContentStoreTest.java
+++ src/test/java/org/atlasapi/persistence/content/cassandra/CassandraContentStoreTest.java
@@ -6,6 +6,7 @@ import com.google.common.collect.Sets;
 import java.util.Arrays;
 import org.atlasapi.media.entity.Clip;
 import org.atlasapi.media.content.Container;
+import org.atlasapi.media.entity.Brand;
 import org.atlasapi.media.entity.Item;
 import org.atlasapi.media.entity.ParentRef;
 import org.atlasapi.media.entity.Publisher;
@@ -55,7 +56,7 @@ public class CassandraContentStoreTest {
         
         Item child1 = new Item("child1", "child1", Publisher.METABROADCAST);
         child1.setId(3L);
-        Container container1 = new Container("container1", "curie1", Publisher.METABROADCAST);
+        Container container1 = new Brand("container1", "curie1", Publisher.METABROADCAST);
         container1.setId(4L);
         container1.setChildRefs(Arrays.asList(child1.childRef()));
         child1.setParentRef(ParentRef.parentRefFrom(container1));
