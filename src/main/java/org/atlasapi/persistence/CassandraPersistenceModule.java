ccdf31ae2e566da196575e7af1c748d001e8005a
Fred van den Driessche <fred@metabroadcast.com>
2013-04-06 19:14:41 +0100

    remove old Cassandra Content classes

--- src/main/java/org/atlasapi/persistence/CassandraPersistenceModule.java
+++ src/main/java/org/atlasapi/persistence/CassandraPersistenceModule.java
@@ -5,7 +5,6 @@ import com.google.common.collect.Lists;
 import com.metabroadcast.common.properties.Configurer;
 
 import org.atlasapi.equiv.CassandraEquivalenceSummaryStore;
-import org.atlasapi.persistence.content.cassandra.CassandraContentStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,12 +19,6 @@ public class CassandraPersistenceModule {
     private final String port = Configurer.get("cassandra.port").get();
     private final String connectionTimeout = Configurer.get("cassandra.connectionTimeout").get();
     private final String requestTimeout = Configurer.get("cassandra.requestTimeout").get();
-
-    @Bean
-    @Qualifier(value = "cassandra")
-    public CassandraContentStore cassandraContentStore() {
-        return new CassandraContentStore(environment, Lists.newArrayList(Splitter.on(',').split(seeds)), Integer.parseInt(port), Runtime.getRuntime().availableProcessors() * 10, Integer.parseInt(connectionTimeout), Integer.parseInt(requestTimeout));
-    }
     
     @Bean
     @Qualifier(value = "cassandra")
