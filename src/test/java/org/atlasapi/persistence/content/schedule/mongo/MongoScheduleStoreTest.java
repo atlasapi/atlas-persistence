b9d8f00e66310b0877e6914908db27be839349be
Tom McAdam <tom@metabroadcast.com>
2013-05-31 15:30:37 +0100

    Use a containsInAnyOrder matcher

--- src/test/java/org/atlasapi/persistence/content/schedule/mongo/MongoScheduleStoreTest.java
+++ src/test/java/org/atlasapi/persistence/content/schedule/mongo/MongoScheduleStoreTest.java
@@ -1,5 +1,6 @@
 package org.atlasapi.persistence.content.schedule.mongo;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.isIn;
@@ -400,6 +401,7 @@ public class MongoScheduleStoreTest {
         
     }
     
+    @SuppressWarnings("unchecked")
     @Test
     public void testResolvesEquivalentItemsForApplicationConfigurationWithPrecendenceEnabled() {
         
@@ -425,7 +427,8 @@ public class MongoScheduleStoreTest {
                 .copyWithPrecedence(ImmutableList.<Publisher>of()));
         
         when(equivalentContentResolver.resolveUris(
-                argThat(hasItems(isIn(uris))), 
+                (Iterable<String>) argThat(containsInAnyOrder(item1.getCanonicalUri(), item2.getCanonicalUri(), 
+                        item3.getCanonicalUri(), item4.getCanonicalUri())), 
                 argThat(is(appConfig.get().getEnabledSources())), 
                 argThat(is(Annotation.defaultAnnotations())), 
                 eq(false)))
