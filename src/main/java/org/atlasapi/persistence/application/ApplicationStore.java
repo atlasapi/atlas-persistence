e96d2110053a9adac74ac5c554f1126ddf0fd41e
liamgh <liam@greenhughes.com>
2013-09-17 17:00:20 +0100

    Move applications persistence package into atlas-persistence

---
+++ src/main/java/org/atlasapi/persistence/application/ApplicationStore.java
@@ -0,0 +1,22 @@
+package org.atlasapi.persistence.application;
+
+import org.atlasapi.application.Application;
+import org.atlasapi.media.common.Id;
+import org.atlasapi.media.entity.Publisher;
+
+import com.google.common.base.Optional;
+
+public interface ApplicationStore {
+
+    Iterable<Application> allApplications();
+
+    Optional<Application> applicationFor(Id id);
+
+    void store(Application application);
+
+    Iterable<Application> applicationsFor(Iterable<Id> ids);
+    
+    Iterable<Application> readersFor(Publisher source);
+    
+    Iterable<Application> writersFor(Publisher source);
+}
