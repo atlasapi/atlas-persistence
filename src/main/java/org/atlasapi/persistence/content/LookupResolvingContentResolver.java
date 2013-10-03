7e21bee72c565deb07861595ae8af45a177103b2
Tom McAdam <tom@metabroadcast.com>
2013-10-03 17:01:50 +0100

    Don't key copy with requested URIs for aliases, since they're not requested by canonical URI

--- src/main/java/org/atlasapi/persistence/content/LookupResolvingContentResolver.java
+++ src/main/java/org/atlasapi/persistence/content/LookupResolvingContentResolver.java
@@ -21,21 +21,19 @@ public class LookupResolvingContentResolver implements ContentResolver {
     @Override
     public ResolvedContent findByCanonicalUris(Iterable<String> canonicalUris) {
         Iterable<LookupEntry> entriesForCanonicalUris = lookupResolver.entriesForCanonicalUris(canonicalUris);
-        return resolveLookupEntries(canonicalUris, entriesForCanonicalUris);
+        return resolveLookupEntries(entriesForCanonicalUris).copyWithAllRequestedUris(canonicalUris);
     }
 
     @Override
     public ResolvedContent findByUris(Iterable<String> uris) {
         Iterable<LookupEntry> entriesForIdentifiers = lookupResolver.entriesForIdentifiers(uris, true);
-        return resolveLookupEntries(uris, entriesForIdentifiers);
+        return resolveLookupEntries(entriesForIdentifiers);
     }
     
-    private ResolvedContent resolveLookupEntries(Iterable<String> uris,
-            Iterable<LookupEntry> entriesForCanonicalUris) {
+    private ResolvedContent resolveLookupEntries(Iterable<LookupEntry> entriesForCanonicalUris) {
         Iterable<LookupRef> lookupRefs = Iterables.transform(entriesForCanonicalUris, LookupEntry.TO_SELF);
         ImmutableSet<LookupRef> resolvedLookups = ImmutableSet.copyOf(Iterables.filter(lookupRefs, Predicates.notNull()));
-        ResolvedContent resolvedContent = knownTypeResolver.findByLookupRefs(resolvedLookups);
-        return resolvedContent.copyWithAllRequestedUris(uris);
+        return knownTypeResolver.findByLookupRefs(resolvedLookups);
     }
 
 }
