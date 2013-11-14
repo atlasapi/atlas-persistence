5491ab17fdd7b8615a90484edeb71e905c8c72fd
Tom McAdam <tom@metabroadcast.com>
2013-11-14 10:58:42 +0000

    Upcoming and available resolvers now return ChildRefs
    Since ChildRefs are no longer held on a brand for generic
    episodes, they must be synthesized.

--- src/main/java/org/atlasapi/persistence/output/UpcomingItemsResolver.java
+++ src/main/java/org/atlasapi/persistence/output/UpcomingItemsResolver.java
@@ -1,12 +1,13 @@
 package org.atlasapi.persistence.output;
 
+import org.atlasapi.media.entity.ChildRef;
 import org.atlasapi.media.entity.Container;
 import org.atlasapi.media.entity.Person;
 
 public interface UpcomingItemsResolver {
 
-    Iterable<String> upcomingItemsFor(Container container);
+    Iterable<ChildRef> upcomingItemsFor(Container container);
     
-    Iterable<String> upcomingItemsFor(Person person);
+    Iterable<ChildRef> upcomingItemsFor(Person person);
 
 }
