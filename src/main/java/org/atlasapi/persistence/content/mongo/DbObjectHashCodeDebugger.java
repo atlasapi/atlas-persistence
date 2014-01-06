2222781b3ad2b5cb54549eb8b863b135bc0cfdbb
Fred van den Driessche <fred@metabroadcast.com>
2014-01-06 13:34:19 +0000

    Improve DbObjectHashCodeDebugger by logging entries in order

--- src/main/java/org/atlasapi/persistence/content/mongo/DbObjectHashCodeDebugger.java
+++ src/main/java/org/atlasapi/persistence/content/mongo/DbObjectHashCodeDebugger.java
@@ -1,9 +1,11 @@
 package org.atlasapi.persistence.content.mongo;
 
 import java.util.Map;
+import java.util.Set;
 
 import org.slf4j.Logger;
 
+import com.google.common.collect.Ordering;
 import com.metabroadcast.common.persistence.mongo.MongoConstants;
 import com.mongodb.DBObject;
 
@@ -13,12 +15,13 @@ public class DbObjectHashCodeDebugger {
     @SuppressWarnings("unchecked")
     public void logHashCodes(DBObject dbObject, Logger log) {
         Object id = dbObject.get(MongoConstants.ID);
-        log.trace("Logging hashes for {}", id);
-        log.trace("Object ID [{}]: hashCode [{}]", dbObject.hashCode());
-        for (Map.Entry<Object, Object> entry : ((Map<Object, Object>) dbObject.toMap()).entrySet()) {
-            Integer hashCode = entry.getValue() != null ? entry.getValue().hashCode() : null;
+        log.trace("Object ID [{}]: hashCode [{}]", id, dbObject.hashCode());
+        Map<String, Object> dbMap = dbObject.toMap();
+        for (String key : Ordering.natural().sortedCopy(dbMap.keySet())) {
+            Object value = dbMap.get(key);
+            Integer hashCode = value != null ? value.hashCode() : null;
             log.trace("Object ID [{}]: Key [{}], hashCode [{}], Value: [{}]", 
-                    new Object[] { id, entry.getKey(), hashCode, entry.getValue() });
+                    new Object[] { id, key, hashCode, value });
         }
         log.trace("Done logging hashes for {}", id);
     }
