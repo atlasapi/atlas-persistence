640c6b47dae2f7e9e9f649a8fc55d880d7c41fb6
Fred van den Driessche <fred@metabroadcast.com>
2014-04-17 15:05:41 +0100

    fix AbstractMessageConfiguration

--- src/main/java/org/atlasapi/messaging/worker/v3/AbstractMessageConfiguration.java
+++ src/main/java/org/atlasapi/messaging/worker/v3/AbstractMessageConfiguration.java
@@ -2,13 +2,14 @@ package org.atlasapi.messaging.worker.v3;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.metabroadcast.common.time.Timestamp;
 
 public abstract class AbstractMessageConfiguration {
 
     @JsonCreator
     AbstractMessageConfiguration(
             @JsonProperty("messageId") String messageId,
-            @JsonProperty("timestamp") Long timestamp,
+            @JsonProperty("timestamp") Timestamp timestamp,
             @JsonProperty("entityId") String entityId,
             @JsonProperty("entityType") String entityType,
             @JsonProperty("entitySource") String entitySource) {
