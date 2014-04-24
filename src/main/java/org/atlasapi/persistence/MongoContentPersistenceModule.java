497d0568a3b62ba3548df43aed798ac0f81a2c3c
Fred van den Driessche <fred@metabroadcast.com>
2014-04-24 16:57:15 +0100

    remove dead messaging code

--- src/main/java/org/atlasapi/persistence/MongoContentPersistenceModule.java
+++ src/main/java/org/atlasapi/persistence/MongoContentPersistenceModule.java
@@ -156,22 +156,13 @@ public class MongoContentPersistenceModule implements ContentPersistenceModule {
     
     private LookupWriter explicitLookupWriter() {
         MongoLookupEntryStore entryStore = new MongoLookupEntryStore(db.collection("lookup"), ReadPreference.primary());
-        LookupWriter lookupWriter = TransitiveLookupWriter.explicitTransitiveLookupWriter(entryStore);
-        return messagingLookupWriter(lookupWriter);
+        return TransitiveLookupWriter.explicitTransitiveLookupWriter(entryStore);
     }
 
     public @Bean
     LookupWriter generatedLookupWriter() {
         MongoLookupEntryStore entryStore = new MongoLookupEntryStore(db.collection("lookup"));
-        LookupWriter lookupWriter = TransitiveLookupWriter.generatedTransitiveLookupWriter(entryStore);
-        return messagingLookupWriter(lookupWriter);
-    }
-
-    private LookupWriter messagingLookupWriter(LookupWriter lookupWriter) {
-//        if (Boolean.valueOf(messagingEnabled)) {
-//            return new MessageQueueingLookupWriter(messagingModule.equivChanges(), lookupWriter);
-//        }
-        return lookupWriter;
+        return TransitiveLookupWriter.generatedTransitiveLookupWriter(entryStore);
     }
 
     public @Primary
