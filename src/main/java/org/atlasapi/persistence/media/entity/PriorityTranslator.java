20c9c7d0adbf671d91a1645ab84661a0a088bf6f
MartinsIrbe <martins.git@irbe.co.uk>
2015-10-26 14:59:35 +0000

    MBST-13037: Added priority reasons translation to and from db object.

--- src/main/java/org/atlasapi/persistence/media/entity/PriorityTranslator.java
+++ src/main/java/org/atlasapi/persistence/media/entity/PriorityTranslator.java
@@ -7,6 +7,7 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import org.atlasapi.media.entity.Described;
 import org.atlasapi.media.entity.Priority;
+import org.atlasapi.media.entity.PriorityScoreReasons;
 
 import java.util.List;
 
@@ -14,25 +15,40 @@ public class PriorityTranslator {
 
     private static final String SCORE_KEY = "score";
     private static final String REASONS_KEY = "reasons";
+    private static final String POSITIVE_REASONS_KEY = "positive";
+    private static final String NEGATIVE_REASONS_KEY = "negative";
     private static final String ITEM_PRIORITY_KEY = "priority";
 
     public Priority getPriority(DBObject dbObject) {
         Double score = null;
-        List<String> scoreReasons = Lists.newArrayList();
+        List<String> positiveScoreReasons = Lists.newArrayList();
+        List<String> negativeScoreReasons = Lists.newArrayList();
         try {
             score = TranslatorUtils.toDouble(dbObject, ITEM_PRIORITY_KEY);
         } catch (ClassCastException e) {
             DBObject priority = TranslatorUtils.toDBObject(dbObject, ITEM_PRIORITY_KEY);
             score = TranslatorUtils.toDouble(priority, SCORE_KEY);
-            List<DBObject> reasons = TranslatorUtils.toDBObjectList(priority, REASONS_KEY);
-            for (Object reason : reasons) {
+            DBObject priorityReasons = TranslatorUtils.toDBObject(priority, REASONS_KEY);
+
+            List<DBObject> positiveReasons = TranslatorUtils.toDBObjectList(priorityReasons, POSITIVE_REASONS_KEY);
+            for (Object reason : positiveReasons) {
                 if (reason != null && reason instanceof String) {
                     String string = (String) reason;
-                    scoreReasons.add(string);
+                    positiveScoreReasons.add(string);
+                }
+            }
+
+            List<DBObject> negativeReasons = TranslatorUtils.toDBObjectList(priorityReasons, NEGATIVE_REASONS_KEY);
+            for (Object reason : positiveReasons) {
+                if (reason != null && reason instanceof String) {
+                    String string = (String) reason;
+                    negativeScoreReasons.add(string);
                 }
             }
         }
-        return new Priority(score, scoreReasons);
+
+        PriorityScoreReasons priorityScoreReasons = new PriorityScoreReasons(positiveScoreReasons, negativeScoreReasons);
+        return new Priority(score, priorityScoreReasons);
     }
 
     public void setPriority(DBObject dbObject, Described entity) {
@@ -43,13 +59,24 @@ public class PriorityTranslator {
         DBObject dbObject = new BasicDBObject();
         TranslatorUtils.from(dbObject, SCORE_KEY, entity.getPriority().getScore());
 
-        BasicDBList list = new BasicDBList();
-        if (entity.getPriority().getReasons() != null) {
-            for (String reason : entity.getPriority().getReasons()) {
-                list.add(reason);
+        DBObject scoreReasons = new BasicDBObject();
+        BasicDBList positiveReasons = new BasicDBList();
+        if (entity.getPriority().getReasons().getPositive() != null) {
+            for (String reason : entity.getPriority().getReasons().getPositive()) {
+                positiveReasons.add(reason);
             }
         }
-        dbObject.put(REASONS_KEY, list);
+        scoreReasons.put(POSITIVE_REASONS_KEY, positiveReasons);
+
+        BasicDBList negativeReasons = new BasicDBList();
+        if (entity.getPriority().getReasons().getNegative() != null) {
+            for (String reason : entity.getPriority().getReasons().getNegative()) {
+                negativeReasons.add(reason);
+            }
+        }
+        scoreReasons.put(NEGATIVE_REASONS_KEY, negativeReasons);
+
+        dbObject.put(REASONS_KEY, scoreReasons);
         return dbObject;
     }
 }
