bb7514530480015451c658adb523c14f00500b53
Timothy <timothy@Timothys-MacBook-Pro.local>
2015-12-10 06:54:11 +0000

    Add serialization and deserialization for new location fields

--- src/main/java/org/atlasapi/persistence/media/entity/LocationTranslator.java
+++ src/main/java/org/atlasapi/persistence/media/entity/LocationTranslator.java
@@ -1,19 +1,28 @@
 package org.atlasapi.persistence.media.entity;
 
+import java.util.List;
+
 import org.atlasapi.media.TransportSubType;
 import org.atlasapi.media.TransportType;
+import org.atlasapi.media.entity.Image;
 import org.atlasapi.media.entity.Location;
 import org.atlasapi.media.entity.Policy;
+import org.atlasapi.media.entity.Quality;
 import org.atlasapi.persistence.ModelTranslator;
 
 import com.metabroadcast.common.persistence.translator.TranslatorUtils;
+import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 
 public class LocationTranslator implements ModelTranslator<Location> {
 	
     public static final String POLICY = "policy";
-    
+    private static final String QUALITY = "quality";
+    private static final String SUBTITLED_LANGUAGES = "subtitledLanguages";
+    private static final String REQUIRED_ENCRYPTION = "requiredEncryption";
+    private static final String VAT = "vat";
+
     private final IdentifiedTranslator descriptionTranslator = new IdentifiedTranslator();
 	private final PolicyTranslator policyTranslator = new PolicyTranslator();
 
@@ -36,12 +45,23 @@ public class LocationTranslator implements ModelTranslator<Location> {
         
         entity.setUri((String) dbObject.get("uri"));
         entity.setLastUpdated(TranslatorUtils.toDateTime(dbObject, IdentifiedTranslator.LAST_UPDATED));
-        
+        entity.setRequiredEncryption(TranslatorUtils.toBoolean(dbObject, REQUIRED_ENCRYPTION));
+        entity.setVat(TranslatorUtils.toDouble(dbObject, VAT));
+
+        decodeSubtitledLanguages(dbObject, entity);
+
         DBObject policyObject = (DBObject) dbObject.get(POLICY);
         if (policyObject != null) {
         	entity.setPolicy(policyTranslator.fromDBObject(policyObject, new Policy()));
         }
         return entity;
+    }
+
+    protected void decodeSubtitledLanguages(DBObject dbObject, Location entity) {
+        if(dbObject.containsField(SUBTITLED_LANGUAGES)) {
+            entity.setSubtitledLanguages(TranslatorUtils.toSet(dbObject, SUBTITLED_LANGUAGES));
+        }
+
     }
 
     private <T extends Enum<T>> T readEnum(Class<T> clazz, DBObject dbObject, String field) {
@@ -60,7 +80,11 @@ public class LocationTranslator implements ModelTranslator<Location> {
         TranslatorUtils.from(dbObject, "embedCode", entity.getEmbedCode());
         TranslatorUtils.from(dbObject, "embedId", entity.getEmbedId());
         TranslatorUtils.from(dbObject, "transportIsLive", entity.getTransportIsLive());
-        
+        TranslatorUtils.from(dbObject, REQUIRED_ENCRYPTION, entity.getRequiredEncryption());
+        TranslatorUtils.from(dbObject, VAT, entity.getVat());
+
+        encodeSubtitledLanguages(dbObject, entity);
+
         if (entity.getTransportType() != null) {
         	TranslatorUtils.from(dbObject, "transportType", entity.getTransportType().toString());
         }
@@ -79,5 +103,11 @@ public class LocationTranslator implements ModelTranslator<Location> {
 			}
         }
         return dbObject;
+    }
+
+    protected void encodeSubtitledLanguages(DBObject dbObject, Location entity) {
+        if(!entity.getSubtitledLanguages().isEmpty()) {
+            TranslatorUtils.fromSet(dbObject, entity.getSubtitledLanguages(), SUBTITLED_LANGUAGES);
+        }
     }
 }
