862ddce023739cf0a762a247f476f76b6050e73f
Jamie Perkins <mail@jperkins.co.uk>
2016-05-26 10:44:41 +0100

    Add Publisher to ChannelQuery

--- src/main/java/org/atlasapi/media/channel/ChannelQuery.java
+++ src/main/java/org/atlasapi/media/channel/ChannelQuery.java
@@ -20,44 +20,47 @@ public class ChannelQuery {
     private final Optional<Set<Long>> channelGroups;
     private final Optional<Set<String>> genres;
     private final Optional<DateTime> advertisedOn;
-    
+    private final Optional<Publisher> publisher;
+
     public static Builder builder() {
         return new Builder();
     }
-    
-    private ChannelQuery(Optional<Publisher> broadcaster, Optional<MediaType> mediaType, 
+
+    private ChannelQuery(Optional<Publisher> broadcaster, Optional<MediaType> mediaType,
             Optional<Publisher> availableFrom, Optional<Set<Long>> channelGroups,
-            Optional<Set<String>> genres, Optional<DateTime> advertisedOn) {
+            Optional<Set<String>> genres, Optional<DateTime> advertisedOn,
+                         Optional<Publisher> publisher) {
                 this.broadcaster = checkNotNull(broadcaster);
                 this.mediaType = checkNotNull(mediaType);
                 this.availableFrom = checkNotNull(availableFrom);
                 this.channelGroups = checkNotNull(channelGroups);
                 this.genres = checkNotNull(genres);
                 this.advertisedOn = checkNotNull(advertisedOn);
+                this.publisher = checkNotNull(publisher);
     }
-    
+
     public Optional<Publisher> getBroadcaster() {
         return broadcaster;
     }
-    
+
     public Optional<MediaType> getMediaType() {
         return mediaType;
     }
-    
+
     public Optional<Publisher> getAvailableFrom() {
         return availableFrom;
     }
-    
+
     public Optional<Set<Long>> getChannelGroups() {
         return channelGroups;
     }
-    
+
     public Optional<Set<String>> getGenres() {
         return genres;
     }
 
-    public Optional<DateTime> getAdvertisedOn() { return advertisedOn; };
-    
+    public Optional<DateTime> getAdvertisedOn() { return advertisedOn; }
+
     @Override
     public String toString() {
         return Objects.toStringHelper(ChannelQuery.class)
@@ -69,12 +72,12 @@ public class ChannelQuery {
                 .add("advertiseOn", advertisedOn)
                 .toString();
     }
-    
+
     @Override
     public int hashCode() {
         return Objects.hashCode(broadcaster, mediaType, availableFrom, channelGroups, genres);
     }
-    
+
     @Override
     public boolean equals(Object that) {
         if (this == that) {
@@ -89,47 +92,48 @@ public class ChannelQuery {
                     && genres.equals(other.genres)
                     && advertisedOn.equals(other.advertisedOn);
         }
-        
+
         return false;
     }
-    
+
     public static class Builder {
-        
+
         private Optional<Publisher> broadcaster = Optional.absent();
         private Optional<MediaType> mediaType = Optional.absent();
         private Optional<Publisher> availableFrom = Optional.absent();
         private Optional<Set<Long>> channelGroups = Optional.absent();
         private Optional<Set<String>> genres = Optional.absent();
         private Optional<DateTime> advertisedOn = Optional.absent();
-        
+        private Optional<Publisher> publisher = Optional.absent();
+
         private Builder() {}
-        
+
         public ChannelQuery build() {
             return new ChannelQuery(broadcaster, mediaType, availableFrom, channelGroups, genres,
-                    advertisedOn);
+                    advertisedOn, publisher);
         }
 
-        
+
         public Builder withBroadcaster(Publisher broadcaster) {
             this.broadcaster = Optional.fromNullable(broadcaster);
             return this;
         }
-        
+
         public Builder withMediaType(MediaType mediaType) {
             this.mediaType = Optional.fromNullable(mediaType);
             return this;
         }
-        
+
         public Builder withAvailableFrom(Publisher availableFrom) {
             this.availableFrom = Optional.fromNullable(availableFrom);
             return this;
         }
-        
+
         public Builder withChannelGroups(Set<Long> channelGroups) {
             this.channelGroups = Optional.fromNullable(channelGroups);
             return this;
         }
-        
+
         public Builder withGenres(Set<String> genres) {
             this.genres = Optional.fromNullable(genres);
             return this;
@@ -137,6 +141,11 @@ public class ChannelQuery {
 
         public Builder withAdvertisedOn(DateTime advertisedOn) {
             this.advertisedOn = Optional.fromNullable(advertisedOn);
+            return this;
+        }
+
+        public Builder withPublisher(Publisher publisher) {
+            this.publisher = Optional.fromNullable(publisher);
             return this;
         }
     }
