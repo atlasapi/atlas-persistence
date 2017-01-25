45d071750f9bda784ff1f9545bcdfe8c99b5ffac
Adam Millican <adamm@metabroadcast.com>
2017-01-25 15:34:01 +0000

    adding test to person translation

--- src/test/java/org/atlasapi/persistence/media/entity/PersonTranslatorTest.java
+++ src/test/java/org/atlasapi/persistence/media/entity/PersonTranslatorTest.java
@@ -4,6 +4,7 @@ import org.atlasapi.media.entity.Person;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
@@ -30,6 +31,33 @@ public class PersonTranslatorTest {
         assertEquals(newPerson.getFamilyName(), resultPerson.getFamilyName());
         assertEquals(newPerson.getGivenName(), resultPerson.getGivenName());
         assertEquals(person.getGender(), resultPerson.getGender());
+
+    }
+
+
+    @Test
+    public void testPersonUpdateWithNewFields() {
+        Person person = new Person();
+
+        person.setFamilyName("familyName");
+        person.setGivenName("givenName");
+        person.setGender("gender");
+
+        Person newPerson = new Person();
+        newPerson.setGivenName("bill");
+        newPerson.setFamilyName("Smith");
+        newPerson.setPseudoForename("billy");
+
+        PersonTranslator personTranslator = new PersonTranslator();
+
+        DBObject dbObject = personTranslator.toDBObject(new BasicDBObject(), person);
+        personTranslator.mainPersonFieldsToDBObject(dbObject, newPerson);
+        Person resultPerson = personTranslator.fromDBObject(dbObject, new Person());
+
+        Assert.assertEquals(newPerson.getFamilyName(), resultPerson.getFamilyName());
+        Assert.assertEquals(newPerson.getGivenName(), resultPerson.getGivenName());
+        Assert.assertEquals(person.getGender(), resultPerson.getGender());
+        Assert.assertEquals(newPerson.getPseudoForename(), resultPerson.getPseudoForename());
 
     }
 }
