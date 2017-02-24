8a74c554cf1f1babb7aa9fcf91a8882421ed5e92
Dragos Pitica <piticadragos@yahoo.com>
2017-02-24 16:00:40 +0000

    reformat class

--- src/main/java/org/atlasapi/persistence/media/entity/IdentifiedTranslator.java
+++ src/main/java/org/atlasapi/persistence/media/entity/IdentifiedTranslator.java
@@ -6,77 +6,48 @@ import org.atlasapi.media.entity.Identified;
 import org.atlasapi.media.entity.LookupRef;
 import org.atlasapi.persistence.ModelTranslator;
 
+import com.metabroadcast.common.persistence.mongo.MongoConstants;
+import com.metabroadcast.common.persistence.translator.TranslatorUtils;
+
 import com.google.common.base.Function;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Sets;
-import com.metabroadcast.common.persistence.mongo.MongoConstants;
-import com.metabroadcast.common.persistence.translator.TranslatorUtils;
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 
 public class IdentifiedTranslator implements ModelTranslator<Identified> {
 
-	public static final String CURIE = "curie";
+    public static final String CURIE = "curie";
 
-	public static final String ALIASES = "aliases";
-	public static final String IDS = "ids";
-	public static final String LAST_UPDATED = "lastUpdated";
-	public static final String EQUIVALENT_TO = "equivalent";
-	public static final String ID = MongoConstants.ID;
-	public static final String CANONICAL_URL = "uri";
-	public static final String TYPE = "type";
+    public static final String ALIASES = "aliases";
+    public static final String IDS = "ids";
+    public static final String LAST_UPDATED = "lastUpdated";
+    public static final String EQUIVALENT_TO = "equivalent";
+    public static final String ID = MongoConstants.ID;
+    public static final String CANONICAL_URL = "uri";
+    public static final String TYPE = "type";
     public static final String PUBLISHER = "publisher";
     public static final String OPAQUE_ID = "aid";
 
-    private final AliasTranslator aliasTranslator = new AliasTranslator();
     private static final LookupRefTranslator lookupRefTranslator = new LookupRefTranslator();
+    private static final Function<DBObject, LookupRef> equivalentFromDbo =
+            input -> lookupRefTranslator.fromDBObject(input, null);
 
-	private boolean useAtlasIdAsId;
+    private static Function<LookupRef, DBObject> equivalentToDbo =
+            input -> lookupRefTranslator.toDBObject(null, input);
 
-	public IdentifiedTranslator() {
-		this(false);
-	}
+    private final AliasTranslator aliasTranslator = new AliasTranslator();
 
-	public IdentifiedTranslator(boolean atlasIdAsId) {
-		this.useAtlasIdAsId = atlasIdAsId;
-	}
+    private boolean useAtlasIdAsId;
 
-	@Override
-    public Identified fromDBObject(DBObject dbObject, Identified description) {
-        if (description == null) {
-            description = new Identified();
-        }
-
-        if(useAtlasIdAsId) {
-            description.setCanonicalUri((String) dbObject.get(CANONICAL_URL));
-            description.setId((Long) dbObject.get(ID));
-        }
-        else {
-            description.setCanonicalUri((String) dbObject.get(ID));
-            description.setId((Long) dbObject.get(OPAQUE_ID));
-        }
-
-        description.setCurie((String) dbObject.get(CURIE));
-        description.setAliasUrls(TranslatorUtils.toSet(dbObject, ALIASES));
-
-        description.setAliases(aliasTranslator.fromDBObjects(TranslatorUtils.toDBObjectList(dbObject, IDS)));
-
-        description.setEquivalentTo(equivalentsFrom(dbObject));
-        description.setLastUpdated(TranslatorUtils.toDateTime(dbObject, LAST_UPDATED));
-        return description;
+    public IdentifiedTranslator() {
+        this(false);
     }
 
-    private Set<LookupRef> equivalentsFrom(DBObject dbObject) {
-        return Sets.newHashSet(Iterables.transform(TranslatorUtils.toDBObjectList(dbObject, EQUIVALENT_TO), equivalentFromDbo));
+    public IdentifiedTranslator(boolean atlasIdAsId) {
+        this.useAtlasIdAsId = atlasIdAsId;
     }
-
-    private static final Function<DBObject, LookupRef> equivalentFromDbo = new Function<DBObject, LookupRef>() {
-        @Override
-        public LookupRef apply(DBObject input) {
-            return lookupRefTranslator.fromDBObject(input, null);
-        }
-    };
 
     @Override
     public DBObject toDBObject(DBObject dbObject, Identified entity) {
@@ -87,8 +58,7 @@ public class IdentifiedTranslator implements ModelTranslator<Identified> {
         if (useAtlasIdAsId) {
             TranslatorUtils.from(dbObject, CANONICAL_URL, entity.getCanonicalUri());
             TranslatorUtils.from(dbObject, ID, entity.getId());
-        }
-        else {
+        } else {
             TranslatorUtils.from(dbObject, ID, entity.getCanonicalUri());
             TranslatorUtils.from(dbObject, OPAQUE_ID, entity.getId());
         }
@@ -104,18 +74,45 @@ public class IdentifiedTranslator implements ModelTranslator<Identified> {
         return dbObject;
     }
 
+    @Override
+    public Identified fromDBObject(DBObject dbObject, Identified description) {
+        if (description == null) {
+            description = new Identified();
+        }
+
+        if (useAtlasIdAsId) {
+            description.setCanonicalUri((String) dbObject.get(CANONICAL_URL));
+            description.setId((Long) dbObject.get(ID));
+        } else {
+            description.setCanonicalUri((String) dbObject.get(ID));
+            description.setId((Long) dbObject.get(OPAQUE_ID));
+        }
+
+        description.setCurie((String) dbObject.get(CURIE));
+        description.setAliasUrls(TranslatorUtils.toSet(dbObject, ALIASES));
+
+        description.setAliases(aliasTranslator.fromDBObjects(TranslatorUtils.toDBObjectList(
+                dbObject,
+                IDS
+        )));
+
+        description.setEquivalentTo(equivalentsFrom(dbObject));
+        description.setLastUpdated(TranslatorUtils.toDateTime(dbObject, LAST_UPDATED));
+        return description;
+    }
+
+    private Set<LookupRef> equivalentsFrom(DBObject dbObject) {
+        return Sets.newHashSet(Iterables.transform(TranslatorUtils.toDBObjectList(
+                dbObject,
+                EQUIVALENT_TO
+        ), equivalentFromDbo));
+    }
+
     private BasicDBList toDBObject(Set<LookupRef> equivalentTo) {
         BasicDBList list = new BasicDBList();
         Iterables.addAll(list, Iterables.transform(equivalentTo, equivalentToDbo));
         return list;
     }
-
-    private static Function<LookupRef, DBObject> equivalentToDbo = new Function<LookupRef, DBObject>() {
-        @Override
-        public DBObject apply(LookupRef input) {
-            return lookupRefTranslator.toDBObject(null, input);
-        }
-    };
 
     public void removeFieldsForHash(DBObject dbObject) {
         dbObject.removeField(LAST_UPDATED);
