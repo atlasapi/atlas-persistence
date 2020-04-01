ba38176320312b76dd9beea60f07ad35c40a6b39
EmiEnache <emi@metabroadcast.com>
2020-04-01 14:27:32 +0100

    Protect against passing null to ProviderTranslator .to

--- src/main/java/org/atlasapi/persistence/media/entity/ProviderTranslator.java
+++ src/main/java/org/atlasapi/persistence/media/entity/ProviderTranslator.java
@@ -1,10 +1,10 @@
 package org.atlasapi.persistence.media.entity;
 
-import com.metabroadcast.common.persistence.translator.ModelTranslator;
 import com.metabroadcast.common.persistence.translator.TranslatorUtils;
 
 import org.atlasapi.media.entity.Provider;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 
 public class ProviderTranslator {
@@ -20,6 +20,10 @@ public class ProviderTranslator {
     }
 
     public DBObject toDBObject(DBObject dbObject, Provider entity) {
+
+        if(dbObject == null) {
+            dbObject = new BasicDBObject();
+        }
         TranslatorUtils.from(dbObject, NAME_KEY, entity.getName());
         TranslatorUtils.from(dbObject, ICON_URL_KEY, entity.getIconUrl());
 
