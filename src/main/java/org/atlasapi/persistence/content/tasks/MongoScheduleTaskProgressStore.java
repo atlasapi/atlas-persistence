f35ac8218377fa5b44ebf58b40b73a9c4aa66ac6
Tristan Beardmore-Gray <tristan.beardmore-gray@hotmail.co.uk>
2020-04-20 16:02:55 +0100

    Moved MongoScheduleTaskProgressStore from atlas to atlas-persistence

---
+++ src/main/java/org/atlasapi/persistence/content/tasks/MongoScheduleTaskProgressStore.java
@@ -0,0 +1,79 @@
+package org.atlasapi.persistence.content.tasks;
+
+import com.metabroadcast.common.persistence.mongo.DatabasedMongo;
+import com.metabroadcast.common.persistence.mongo.MongoUpdateBuilder;
+import com.metabroadcast.common.persistence.translator.TranslatorUtils;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import org.atlasapi.media.entity.Publisher;
+import org.atlasapi.persistence.content.ContentCategory;
+import org.atlasapi.persistence.content.listing.ContentListingProgress;
+
+import static com.metabroadcast.common.persistence.mongo.MongoBuilders.update;
+import static com.metabroadcast.common.persistence.mongo.MongoBuilders.where;
+import static com.metabroadcast.common.persistence.mongo.MongoConstants.ID;
+import static com.metabroadcast.common.persistence.mongo.MongoConstants.SINGLE;
+import static com.metabroadcast.common.persistence.mongo.MongoConstants.UPSERT;
+
+public class MongoScheduleTaskProgressStore implements ScheduleTaskProgressStore {
+    
+    private static final String SCHEDULING_COLLECTION_NAME = "scheduling";
+    
+    private static final String START = "start";
+    private static final String PUBLISHER = "publisher";
+    private static final String LAST_ID = "lastId";
+    private static final String CATEGORY = "category";
+    
+    private final DBCollection collection;
+
+    public MongoScheduleTaskProgressStore(DatabasedMongo mongo) {
+        collection = mongo.collection(SCHEDULING_COLLECTION_NAME);
+    }
+
+    @Override
+    public ContentListingProgress progressForTask(String taskName) {
+        DBObject existing = collection.findOne(taskName);
+        return existing != null ? fromDbo(existing) : ContentListingProgress.START;
+    }
+
+    private ContentListingProgress fromDbo(DBObject progress) {
+        String lastId = TranslatorUtils.toString(progress, LAST_ID);
+        
+        if(START.equals(lastId)) {
+            return ContentListingProgress.START;
+        }
+        
+        String tableName = TranslatorUtils.toString(progress, CATEGORY);
+        ContentCategory category = tableName == null ? null : ContentCategory.valueOf(tableName);
+        
+        String pubKey = TranslatorUtils.toString(progress, PUBLISHER);
+        Publisher publisher = pubKey == null ? null : Publisher.fromKey(pubKey).valueOrNull();
+        
+        return new ContentListingProgress(category, publisher, lastId);
+    }
+
+    @Override
+    public void storeProgress(String taskName, ContentListingProgress progress) {
+        MongoUpdateBuilder update = update().setField(LAST_ID, progress.getUri() == null ? START : progress.getUri());
+        
+        if(progress.getCategory() != null) {
+            update.setField(CATEGORY, progress.getCategory().toString());
+        } else {
+            update.unsetField(CATEGORY);
+        }
+        
+        if(progress.getPublisher() != null) {
+            update.setField(PUBLISHER, progress.getPublisher().key());
+        } else {
+            update.unsetField(PUBLISHER);
+        }
+        
+        collection.update(
+                where().fieldEquals(ID, taskName).build(),
+                update.build(),
+                UPSERT,
+                SINGLE
+        );
+    }
+
+}
